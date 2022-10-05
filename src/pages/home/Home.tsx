75e67c9ab416a289a1345abe2a1e891745d1ec6e
Federico Piacitelli <f.piacitelli@gmail.com>
2022-10-05 18:52:14 +0200

    add loading layer when fetch is pending

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,11 +2,13 @@ import { useState, useEffect } from 'react';
 import { Box, Typography, Link, useTheme } from '@mui/material';
 import { TitleBox } from '@pagopa/selfcare-common-frontend';
 import { useTranslation, Trans } from 'react-i18next';
+import useLoading from '@pagopa/selfcare-common-frontend/hooks/useLoading';
 import AddIcon from '@mui/icons-material/Add';
 import { ButtonNaked } from '@pagopa/mui-italia';
 import { useErrorDispatcher, useUserNotify } from '@pagopa/selfcare-common-frontend';
 import { useAppSelector } from '../../redux/hooks';
 import { partiesSelectors } from '../../redux/slices/partiesSlice';
+import { LOADING_TASK_API_KEY_GENERATION } from '../../utils/constants';
 import HomePageCard from './HomePageCard';
 import {
   getInstitutionApiKeys,
@@ -25,89 +27,102 @@ const Home = () => {
 
   const addNotify = useUserNotify();
   const addError = useErrorDispatcher();
+  const setLoading = useLoading(LOADING_TASK_API_KEY_GENERATION);
 
   const selectedParty = useAppSelector(partiesSelectors.selectPartySelected);
 
   useEffect(() => {
     if (selectedParty) {
-      void getInstitutionApiKeys(selectedParty.partyId).then((data) => {
-        setPrimaryKey(data.primaryKey);
-        setSecondaryKey(data.secondaryKey);
-        if (data.primaryKey !== '' && data.secondaryKey !== '') {
-          setApiKeyPresent(true);
-        } else {
-          setApiKeyPresent(false);
-        }
-      });
+      setLoading(true);
+      void getInstitutionApiKeys(selectedParty.partyId)
+        .then((data) => {
+          setPrimaryKey(data.primaryKey);
+          setSecondaryKey(data.secondaryKey);
+          if (data.primaryKey !== '' && data.secondaryKey !== '') {
+            setApiKeyPresent(true);
+          } else {
+            setApiKeyPresent(false);
+          }
+        })
+        .finally(() => setLoading(false));
     }
   }, [selectedParty]);
 
   const createKeys = () => {
     if (selectedParty) {
-      void createInstitutionApiKeys(selectedParty.partyId).then((data) => {
-        setPrimaryKey(data.primaryKey);
-        setSecondaryKey(data.secondaryKey);
-        setApiKeyPresent(true);
-      });
+      setLoading(true);
+      void createInstitutionApiKeys(selectedParty.partyId)
+        .then((data) => {
+          setPrimaryKey(data.primaryKey);
+          setSecondaryKey(data.secondaryKey);
+          setApiKeyPresent(true);
+        })
+        .finally(() => setLoading(false));
     }
   };
 
   const regenPrimaryKey = () => {
     if (selectedParty) {
-      regeneratePrimaryKey(selectedParty.partyId).then(
-        () => {
-          void getInstitutionApiKeys(selectedParty.partyId).then((data) =>
-            setPrimaryKey(data.primaryKey)
-          );
-          addNotify({
-            id: 'ACTION_ON_REGENERATE_PRIMARY_KEY',
-            title: '',
-            message: t('homepage.apiPresent.regeneratePrimaryKey'),
-            component: 'Toast',
-          });
-        },
-        (reason) => {
-          addError({
-            component: 'Toast',
-            id: 'ACTION_ON_REGENERATE_PRIMARY_KEY',
-            displayableTitle: t('homepage.apiPresent.errorRegeneratePrimaryKey'),
-            techDescription: `C'è stato un errore durante la rigenerazione della chiave primaria`,
-            blocking: false,
-            error: reason,
-            toNotify: true,
-            displayableDescription: '',
-          });
-        }
-      );
+      setLoading(true);
+      regeneratePrimaryKey(selectedParty.partyId)
+        .then(
+          () => {
+            void getInstitutionApiKeys(selectedParty.partyId).then((data) =>
+              setPrimaryKey(data.primaryKey)
+            );
+            addNotify({
+              id: 'ACTION_ON_REGENERATE_PRIMARY_KEY',
+              title: '',
+              message: t('homepage.apiPresent.regeneratePrimaryKey'),
+              component: 'Toast',
+            });
+          },
+          (reason) => {
+            addError({
+              component: 'Toast',
+              id: 'ACTION_ON_REGENERATE_PRIMARY_KEY',
+              displayableTitle: t('homepage.apiPresent.errorRegeneratePrimaryKey'),
+              techDescription: `C'è stato un errore durante la rigenerazione della chiave primaria`,
+              blocking: false,
+              error: reason,
+              toNotify: true,
+              displayableDescription: '',
+            });
+          }
+        )
+        .finally(() => setLoading(false));
     }
   };
   const regenSecondaryKey = () => {
     if (selectedParty) {
-      regenerateSecondaryKey(selectedParty.partyId).then(
-        () => {
-          void getInstitutionApiKeys(selectedParty.partyId).then((data) =>
-            setSecondaryKey(data.secondaryKey)
-          );
-          addNotify({
-            id: 'ACTION_ON_REGENERATE_SECONDARY_KEY',
-            title: '',
-            message: t('homepage.apiPresent.regenerateSecondaryKey'),
-            component: 'Toast',
-          });
-        },
-        (reason) => {
-          addError({
-            component: 'Toast',
-            id: 'ACTION_ON_REGENERATE_PRIMARY_KEY',
-            displayableTitle: t('homepage.apiPresent.errorRegenerateSecondaryKey'),
-            techDescription: `C'è stato un errore durante la rigenerazione della chiave secondaria`,
-            blocking: false,
-            error: reason,
-            toNotify: true,
-            displayableDescription: '',
-          });
-        }
-      );
+      setLoading(true);
+      regenerateSecondaryKey(selectedParty.partyId)
+        .then(
+          () => {
+            void getInstitutionApiKeys(selectedParty.partyId).then((data) =>
+              setSecondaryKey(data.secondaryKey)
+            );
+            addNotify({
+              id: 'ACTION_ON_REGENERATE_SECONDARY_KEY',
+              title: '',
+              message: t('homepage.apiPresent.regenerateSecondaryKey'),
+              component: 'Toast',
+            });
+          },
+          (reason) => {
+            addError({
+              component: 'Toast',
+              id: 'ACTION_ON_REGENERATE_PRIMARY_KEY',
+              displayableTitle: t('homepage.apiPresent.errorRegenerateSecondaryKey'),
+              techDescription: `C'è stato un errore durante la rigenerazione della chiave secondaria`,
+              blocking: false,
+              error: reason,
+              toNotify: true,
+              displayableDescription: '',
+            });
+          }
+        )
+        .finally(() => setLoading(false));
     }
   };
 

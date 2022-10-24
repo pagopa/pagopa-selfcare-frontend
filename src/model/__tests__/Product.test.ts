a43c77af76a085985cdecc81a44fd56044a0e558
Federico Piacitelli <f.piacitelli@gmail.com>
2022-10-24 12:53:29 +0200

    fix Party and Product tests

--- src/model/__tests__/Product.test.ts
+++ src/model/__tests__/Product.test.ts
@@ -5,41 +5,25 @@ test('Test institutionInfo2Party', () => {
   const date = new Date();
 
   const productResource: ProductsResource = {
-    logo: 'https://selcdcheckoutsa.z6.web.core.windows.net/resources/products/prod-pagopa/logo.png',
+    description: 'Pagamenti pagoPA description',
     id: '3',
     title: 'Pagamenti pagoPA',
-    description: 'Pagamenti pagoPA description',
-    authorized: true,
-    status: StatusEnum.ACTIVE,
     urlBO: 'http://pagopa/bo',
-    activatedAt: date,
     urlPublic: 'http://pagopa/public',
-    userRole: 'LIMITED',
-    children: [{ id: 'subProductId', title: 'Premium', status: StatusEnum.ACTIVE }],
-    imageUrl:
-      'https://selcdcheckoutsa.z6.web.core.windows.net/resources/products/default/depict-image.jpeg',
   };
 
   const product = productResource2Product(productResource);
   expect(product).toStrictEqual({
-    logo: 'https://selcdcheckoutsa.z6.web.core.windows.net/resources/products/prod-pagopa/logo.png',
+    description: 'Pagamenti pagoPA description',
     id: '3',
     title: 'Pagamenti pagoPA',
-    description: 'Pagamenti pagoPA description',
+    urlBO: 'http://pagopa/bo',
+    urlPublic: 'http://pagopa/public',
+    selfcareRole: undefined,
+    roles: [],
     authorized: true,
     status: 'ACTIVE',
-    urlBO: 'http://pagopa/bo',
-    activationDateTime: date,
-    urlPublic: 'http://pagopa/public',
-    roles: [
-      {
-        partyRole: undefined,
-        roleKey: 'LIMITED',
-      },
-    ],
-    selfcareRole: undefined,
-    subProducts: [{ id: 'subProductId', title: 'Premium', status: 'ACTIVE' }],
-    imageUrl:
-      'https://selcdcheckoutsa.z6.web.core.windows.net/resources/products/default/depict-image.jpeg',
+    imageUrl: '',
+    subProducts: [],
   });
 });

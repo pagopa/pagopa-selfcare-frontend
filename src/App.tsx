664e5a15ed217ba0c6053c4369948d9807a49e34
Federico Piacitelli <f.piacitelli@gmail.com>
2022-10-28 13:56:14 +0200

    feat: add TOS component

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,69 @@ import {
   UserNotifyHandle,
 } from '@pagopa/selfcare-common-frontend';
 import { Redirect, Route, Switch } from 'react-router-dom';
+import { Box, Typography, Link } from '@mui/material';
+import { TOSAgreement } from '@pagopa/mui-italia';
+import { Trans, useTranslation } from 'react-i18next';
 import withLogin from './decorators/withLogin';
 import Layout from './components/Layout/Layout';
 import routes from './routes';
 import Home from './pages/home/Home';
 import withSelectedPartyProducts from './decorators/withSelectedPartyProducts';
 import Auth from './pages/auth/Auth';
+import { TOS } from './pages/tos/TOS';
+import { useTOSAgreementLocalStorage } from './hooks/useTOSAgreementLocalStorage';
+
 // import Wizard from './components/Wizard/Wizard';
 
 const SecuredRoutes = withLogin(
-  withSelectedPartyProducts(() => (
-    <Layout>
-      <Switch>
-        <Route path={routes.HOME} exact={true}>
-          <Home />
-        </Route>
-        {/* <Route path={routes.WIZARD} exact={true}>
-          <Wizard />
-        </Route> */}
-        <Route path="*">
-          <Redirect to={routes.HOME} />
-        </Route>
-      </Switch>
-    </Layout>
-  ))
+  withSelectedPartyProducts(() => {
+    const { isTOSAccepted, acceptTOS } = useTOSAgreementLocalStorage();
+    const { t } = useTranslation();
+
+    return (
+      <Layout>
+        {!isTOSAccepted && window.location.pathname !== routes.TOS ? (
+          <Box width="100%" px={2}>
+            <TOSAgreement
+              productName={t('tos.title')}
+              description={t('tos.description')}
+              onConfirm={() => acceptTOS()}
+            >
+              <Typography sx={{ px: 8 }} color="text.secondary">
+                <Trans i18nKey="tos.termsDescription">
+                  Entrando dichiari di aver letto e accettato l’Informativa Privacy e i Termini e
+                  condizioni d’uso di PagoPA
+                  <Link
+                    sx={{ color: 'primary.main', cursor: 'pointer', textDecoration: 'none' }}
+                    onClick={() => {
+                      window.location.assign(routes.TOS);
+                    }}
+                  >
+                    <strong> Accedi</strong>
+                  </Link>
+                </Trans>
+              </Typography>
+            </TOSAgreement>
+          </Box>
+        ) : (
+          <Switch>
+            <Route path={routes.HOME} exact={true}>
+              <Home />
+            </Route>
+            {/* <Route path={routes.WIZARD} exact={true}>
+        <Wizard />
+      </Route> */}
+            <Route path={routes.TOS} exact={true}>
+              <TOS />
+            </Route>
+            <Route path="*">
+              <Redirect to={routes.HOME} />
+            </Route>
+          </Switch>
+        )}
+      </Layout>
+    );
+  })
 );
 
 const App = () => (

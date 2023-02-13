b44805f8dba314f60b76275f668f6ccd23fcbda3
Federico Piacitelli <f.piacitelli@gmail.com>
2023-02-13 15:35:43 +0100

    PASELC-46 add channel detail

--- src/pages/channels/detail/ChannelDetailPage.tsx
+++ src/pages/channels/detail/ChannelDetailPage.tsx
@@ -1,27 +1,231 @@
-import { Box, Grid } from '@mui/material';
+import { ArrowBack, ManageAccounts } from '@mui/icons-material';
+import { Box, Chip, Divider, Grid } from '@mui/material';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import { ButtonNaked, theme } from '@pagopa/mui-italia';
 import { TitleBox } from '@pagopa/selfcare-common-frontend';
 
 import { useTranslation } from 'react-i18next';
-import { useParams } from 'react-router';
+import { useHistory, useParams } from 'react-router';
+import ROUTES from '../../../routes';
 
 const ChannelDetailPage = () => {
   const { t } = useTranslation();
+  const history = useHistory();
+
   const { channelId } = useParams<{ channelId: string }>();
 
+  const goBack = () => history.push(ROUTES.CHANNELS);
+
   return (
-    <Grid container item xs={12} sx={{ backgroundColor: 'background.paper' }}>
-      <Box width="100%">
-        <TitleBox
-          title={t('channelsPage.title')}
-          subTitle={t('channelsPage.subtitle')}
-          mbTitle={2}
-          mtTitle={4}
-          mbSubTitle={3}
-          variantTitle="h4"
-          variantSubTitle="body1"
-        />
-      </Box>
-      <Box>ChannelId:{channelId}</Box>
+    <Grid container justifyContent={'center'}>
+      <Grid item p={3} xs={8}>
+        <Stack direction="row">
+          <ButtonNaked
+            size="small"
+            component="button"
+            onClick={goBack}
+            startIcon={<ArrowBack />}
+            sx={{ color: 'primary.main', mr: '20px' }}
+            weight="default"
+          >
+            {t('general.exit')}
+          </ButtonNaked>
+          <Breadcrumbs>
+            <Typography>{t('general.Channels')}</Typography>
+            <Typography color={'#A2ADB8'}>
+              {t('channelDetailPage.detail')} {channelId}
+            </Typography>
+          </Breadcrumbs>
+        </Stack>
+        <Grid container mt={3}>
+          <Grid item xs={6}>
+            <TitleBox title={channelId} mbTitle={2} variantTitle="h4" variantSubTitle="body1" />
+            <Typography mb={5}>
+              {t('channelDetailPage.createdOn')}{' '}
+              <Typography component={'span'} fontWeight={600}>
+                17/01/2023
+              </Typography>
+            </Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Stack spacing={2} direction="row" flexWrap={'wrap'} justifyContent={'flex-end'}>
+              <Button
+                color={'error'}
+                style={{ color: theme.palette.error.dark, borderColor: theme.palette.error.dark }}
+                variant="outlined"
+                onClick={goBack}
+              >
+                {t('channelDetailPage.disable')}
+              </Button>
+              <Button color="primary" variant="outlined" onClick={goBack}>
+                {t('channelDetailPage.duplicate')}
+              </Button>
+              <Button variant="contained">{t('channelDetailPage.edit')}</Button>
+            </Stack>
+          </Grid>
+        </Grid>
+
+        <Paper
+          elevation={8}
+          sx={{
+            borderRadius: 4,
+            p: 4,
+          }}
+        >
+          <Grid container alignItems={'center'} spacing={0} mb={2}>
+            <Grid item xs={3}>
+              <Typography variant="subtitle2">{t('channelDetailPage.state')}</Typography>
+            </Grid>
+            <Grid item xs={9} textAlign="right">
+              <Chip size="medium" label={t('channelDetailPage.states.revision')} />
+            </Grid>
+          </Grid>
+          <Typography variant="h6" mb={3}>
+            {t('channelDetailPage.channelConfiguration')}
+          </Typography>
+          <Divider></Divider>
+
+          <Box mt={5}>
+            <Grid container spacing={2}>
+              <Grid container item alignContent="center" spacing={2} pb={4}>
+                <Grid item xs={12}>
+                  <Typography variant="sidenav">{t('channelDetailPage.registry')}</Typography>
+                </Grid>
+                <Grid item xs={3}>
+                  <Typography variant="body2">{t('channelDetailPage.pspBrokerCode')}</Typography>
+                </Grid>
+                <Grid item xs={9}>
+                  <Typography variant="body2" fontWeight={600}>
+                    {channelId}
+                  </Typography>
+                </Grid>
+                <Grid item xs={3}>
+                  <Typography variant="body2">{t('channelDetailPage.companyName')}</Typography>
+                </Grid>
+                <Grid item xs={9}>
+                  <Typography variant="body2" fontWeight={600}>
+                    {channelId}
+                  </Typography>
+                </Grid>
+                <Grid item xs={3}>
+                  <Typography variant="body2">{t('channelDetailPage.idChannel')}</Typography>
+                </Grid>
+                <Grid item xs={9}>
+                  <Typography variant="body2" fontWeight={600}>
+                    {channelId}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12} mt={2}>
+                  <Typography variant="sidenav">{t('channelDetailPage.redirect')}</Typography>
+                </Grid>
+                <Grid item xs={3}>
+                  <Typography variant="body2">{t('channelDetailPage.redirectProtocol')}</Typography>
+                </Grid>
+                <Grid item xs={9}>
+                  <Typography variant="body2" fontWeight={600}>
+                    {channelId}
+                  </Typography>
+                </Grid>
+                <Grid item xs={3}>
+                  <Typography variant="body2">{t('channelDetailPage.redirectPort')}</Typography>
+                </Grid>
+                <Grid item xs={9}>
+                  <Typography variant="body2" fontWeight={600}>
+                    {channelId}
+                  </Typography>
+                </Grid>
+                <Grid item xs={3}>
+                  <Typography variant="body2">{t('channelDetailPage.redirectUrl')}</Typography>
+                </Grid>
+                <Grid item xs={9}>
+                  <Typography variant="body2" fontWeight={600}>
+                    {channelId}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12} mt={2}>
+                  <Typography variant="sidenav">{t('channelDetailPage.target')}</Typography>
+                </Grid>
+                <Grid item xs={3}>
+                  <Typography variant="body2">{t('channelDetailPage.targetAddress')}</Typography>
+                </Grid>
+                <Grid item xs={9}>
+                  <Typography variant="body2" fontWeight={600}>
+                    {channelId}
+                  </Typography>
+                </Grid>
+                <Grid item xs={3}>
+                  <Typography variant="body2">{t('channelDetailPage.targetService')}</Typography>
+                </Grid>
+                <Grid item xs={9}>
+                  <Typography variant="body2" fontWeight={600}>
+                    {channelId}
+                  </Typography>
+                </Grid>
+                <Grid item xs={3}>
+                  <Typography variant="body2">{t('channelDetailPage.targetPort')}</Typography>
+                </Grid>
+                <Grid item xs={9}>
+                  <Typography variant="body2" fontWeight={600}>
+                    {channelId}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12} mt={2}>
+                  <Typography variant="sidenav">{t('channelDetailPage.paymentType')}</Typography>
+                </Grid>
+                <Grid item xs={12} mb={3} mt={2}>
+                  <Chip color="primary" label="Bancomat PAY" />
+                </Grid>
+                <Grid item xs={6} alignItems={'center'}>
+                  <Typography variant="sidenav">{t('channelDetailPage.associatedPsp')}</Typography>
+                </Grid>
+                <Grid
+                  item
+                  textAlign={'right'}
+                  display={'flex'}
+                  alignItems={'center'}
+                  justifyContent={'flex-end'}
+                  xs={6}
+                >
+                  <ButtonNaked color="primary" endIcon={<ManageAccounts />} size="medium">
+                    {t('channelDetailPage.managePsp')}
+                  </ButtonNaked>
+                </Grid>
+                <Grid item xs={3}>
+                  <Typography variant="body2">{t('channelDetailPage.associated')}</Typography>
+                </Grid>
+                <Grid item xs={9}>
+                  <Typography variant="body2" fontWeight={600}>
+                    {channelId}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12} mt={2}>
+                  <Typography variant="sidenav">{t('channelDetailPage.changes')}</Typography>
+                </Grid>
+                <Grid item xs={3}>
+                  <Typography variant="body2">{t('channelDetailPage.lastChange')}</Typography>
+                </Grid>
+                <Grid item xs={9}>
+                  <Typography variant="body2" fontWeight={600}>
+                    {channelId}
+                  </Typography>
+                </Grid>
+                <Grid item xs={3}>
+                  <Typography variant="body2">{t('channelDetailPage.operatedBy')}</Typography>
+                </Grid>
+                <Grid item xs={9}>
+                  <Typography variant="body2" fontWeight={600}>
+                    {channelId}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Box>
+        </Paper>
+      </Grid>
     </Grid>
   );
 };

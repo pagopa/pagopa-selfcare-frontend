bc4c2f3fb3fbbff010721e5bdd5734712254f524
FILIPPO GABRIELE MAIOCCHI <fmaiocch@emeal.nttdata.com>
2023-03-23 15:38:14 +0100

    add control for calling only mocked station services

--- src/services/stationService.ts
+++ src/services/stationService.ts
@@ -9,7 +9,7 @@ import {
 import { createStationMocked, mockedStation } from './__mocks__/stationService';
 
 export const createStation = (station: StationOnCreation): Promise<StationDetailsDto> => {
-  if (process.env.REACT_APP_API_MOCK_PORTAL === 'true') {
+  if (process.env.REACT_APP_API_MOCK_PORTAL !== 'true') {
     return createStationMocked(mockedStation);
   }
   return PortalApi.createStation(station).then((resources) => resources);
@@ -17,14 +17,14 @@ export const createStation = (station: StationOnCreation): Promise<StationDetail
 
 export const getStations = (page: number): Promise<StationsResource> => {
   /* istanbul ignore if */
-  if (process.env.REACT_APP_API_MOCK_PORTAL === 'true') {
+  if (process.env.REACT_APP_API_MOCK_PORTAL !== 'true') {
     return getStationsMocked(0);
   }
   return PortalApi.getStations(page).then((resource) => resource);
 };
 
 export const getStationCode = (code: string): Promise<string> => {
-  if (process.env.REACT_APP_API_MOCK_PORTAL === 'true') {
+  if (process.env.REACT_APP_API_MOCK_PORTAL !== 'true') {
     return getStationCodeMocked(code);
   }
   return PortalApi.getStationCode(code).then((resource) => resource);

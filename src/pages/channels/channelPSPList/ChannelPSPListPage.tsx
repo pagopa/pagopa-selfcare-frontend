543cbdfb25b1ff4f9898a98dbd22a704df5ee9c1
Federico Piacitelli <f.piacitelli@gmail.com>
2023-04-06 12:25:43 +0200

    fix @typescript-eslint/restrict-plus-operands error

--- src/pages/channels/channelPSPList/ChannelPSPListPage.tsx
+++ src/pages/channels/channelPSPList/ChannelPSPListPage.tsx
@@ -1,4 +1,3 @@
-// eslint-disable @typescript-eslint/restrict-plus-operands
 import { ArrowBack, FileDownload } from '@mui/icons-material';
 import { Alert, Box, Button, Breadcrumbs, Grid, Stack, Typography } from '@mui/material';
 import { ButtonNaked } from '@pagopa/mui-italia';
@@ -31,10 +30,9 @@ const ChannelPSPListPage = () => {
     const rows = mockedChannelPSPs.channels;
     const headers = ['Nome PSP', 'Referente'];
 
-    const csvContent =
-      headers.join(';') +
-      '\n' +
-      rows.map((e) => [e.channel_code, e.broker_description].join(';')).join('\n');
+    const csvContent = `${headers.join(';')}\n${rows
+      .map((e) => [e.channel_code, e.broker_description].join(';'))
+      .join('\n')}`;
     const blob = new Blob([csvContent], { type: 'text/csv' });
     const url = window.URL.createObjectURL(blob);
     const a = document.createElement('a');

a99820780a3d3afa7a0aa7d4721582f81ea9b64b
FILIPPO GABRIELE MAIOCCHI <fmaiocch@emeal.nttdata.com>
2023-04-14 19:29:50 +0200

    udpated NodeSignInEcForm and add updateCreditorInstitutionDetails

--- src/pages/dashboard/nodeSignIn/NodeSignInPage.tsx
+++ src/pages/dashboard/nodeSignIn/NodeSignInPage.tsx
@@ -1,16 +1,18 @@
 import { ArrowBack } from '@mui/icons-material';
 import { Breadcrumbs, Grid, Stack, Typography } from '@mui/material';
 import { ButtonNaked } from '@pagopa/mui-italia';
-import { TitleBox } from '@pagopa/selfcare-common-frontend';
-import { useEffect } from 'react';
+import { TitleBox, useErrorDispatcher, useLoading } from '@pagopa/selfcare-common-frontend';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useHistory } from 'react-router';
 import { useAppSelector } from '../../../redux/hooks';
 import { partiesSelectors } from '../../../redux/slices/partiesSlice';
-
 import ROUTES from '../../../routes';
-import NodeSignInECForm from './NodeSignInECForm';
+import { CreditorInstitutionDetailsResource } from '../../../api/generated/portal/CreditorInstitutionDetailsResource';
+import { getCreditorInstitutionDetails } from '../../../services/nodeService';
+import { LOADING_TASK_DASHBOARD_GET_EC_PSP_DETAILS } from '../../../utils/constants';
 import NodeSignInPSPForm from './NodeSignInPSPForm';
+import NodeSignInECForm from './NodeSignInECForm';
 
 const NodeSignInPage = () => {
   const { t } = useTranslation();
@@ -18,9 +20,36 @@ const NodeSignInPage = () => {
   const selectedParty = useAppSelector(partiesSelectors.selectPartySelected);
   const goBack = () => history.push(ROUTES.HOME);
 
+  const addError = useErrorDispatcher();
+  const setLoading = useLoading(LOADING_TASK_DASHBOARD_GET_EC_PSP_DETAILS);
+  const [ecNodeData, setEcNodeData] = useState<CreditorInstitutionDetailsResource>();
+
   useEffect(() => {
-    // load data
-  }, []);
+    if (
+      selectedParty !== undefined &&
+      selectedParty.pspData === undefined &&
+      selectedParty.institutionType !== 'PSP'
+    ) {
+      setLoading(true);
+      getCreditorInstitutionDetails(selectedParty.fiscalCode)
+        .then((res) => {
+          setEcNodeData(res);
+        })
+        .catch((reason) => {
+          addError({
+            id: 'NODE_SIGN_IN_PAGE_EC_DETAILS',
+            blocking: false,
+            error: reason as Error,
+            techDescription: `An error occurred while getting ec details`,
+            toNotify: true,
+            displayableTitle: t('dashboardPage.registrationData.ecDetailsErrorMessageTitle'),
+            displayableDescription: t('dashboardPage.registrationData.ecDetailsErrorMessageDesc'),
+            component: 'Toast',
+          });
+        })
+        .finally(() => setLoading(false));
+    }
+  }, [selectedParty]);
 
   return (
     <Grid container justifyContent={'center'}>
@@ -57,7 +86,7 @@ const NodeSignInPage = () => {
         {selectedParty?.pspData ? (
           <NodeSignInPSPForm goBack={goBack} />
         ) : (
-          <NodeSignInECForm goBack={goBack} />
+          <NodeSignInECForm goBack={goBack} ecNodeData={ecNodeData} />
         )}
       </Grid>
     </Grid>
--- src/services/__mocks__/nodeService.ts
+++ src/services/__mocks__/nodeService.ts
@@ -1,6 +1,7 @@
 import { CreditorInstitutionDetailsResource } from '../../api/generated/portal/CreditorInstitutionDetailsResource';
 import { CreditorInstitutionDto } from '../../api/generated/portal/CreditorInstitutionDto';
 import { PaymentServiceProviderDetailsResource } from '../../api/generated/portal/PaymentServiceProviderDetailsResource';
+import { UpdateCreditorInstitutionDto } from '../../api/generated/portal/UpdateCreditorInstitutionDto';
 import { NodeOnSignInPSP } from '../../model/Node';
 import { PSPDirectDTO } from '../../model/PSP';
 
@@ -48,6 +49,22 @@ const ecDirect: CreditorInstitutionDetailsResource = {
   reportingZip: false,
 };
 
+const ecDirectUpdated: UpdateCreditorInstitutionDto = {
+  address: {
+    city: 'Monza',
+    countryCode: 'Via Liberta 64',
+    location: 'MB',
+    taxDomicile: 'Via Ernesto Calindri 4',
+    zipCode: '20900',
+  },
+  businessName: 'businessName',
+  creditorInstitutionCode: 'creditorInstitutionCode',
+  enabled: true,
+  pspPayment: false,
+  reportingFtp: false,
+  reportingZip: false,
+};
+
 export const createPSPDirect = (_psp: NodeOnSignInPSP): Promise<PSPDirectDTO> =>
   new Promise((resolve) => resolve(pspDirect));
 
@@ -60,3 +77,8 @@ export const createECDirect = (
 
 export const getECDetails = (_ecCode: string): Promise<CreditorInstitutionDetailsResource> =>
   new Promise((resolve) => resolve(ecDirect));
+
+export const updateECDirect = (
+  _ec: UpdateCreditorInstitutionDto
+): Promise<CreditorInstitutionDetailsResource> =>
+  new Promise((resolve) => resolve(ecDirectUpdated));

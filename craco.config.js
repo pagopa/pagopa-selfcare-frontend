4a48cfd4d8a90f4065a52ca208dd434cbbc918e3
fp <f.piacitelli@gmail.com>
2023-07-21 22:51:12 +0200

    add *.test.tsx to coverage exclusion

--- craco.config.js
+++ craco.config.js
@@ -14,22 +14,4 @@ module.exports = {
       ignoreWarnings: [/Failed to parse source map/],
     },
   },
-  
-  // jest:{
-  //   configure: {
-  //     coveragePathIgnorePatterns: [
-  //       "src/index.js",
-  //       "src/bootstrap.tsx",
-  //       "src/consentAndAnalyticsConfiguration.ts",
-  //       "src/reportWebVitals.ts",
-  //       "src/api/generated",
-  //       "src/locale/it.ts",
-  //     ],
-  //     collectCoverageFrom: [
-  //       "src/**/*.{js,jsx,ts,tsx}",
-  //       "!src/index.js",
-  //       "!*.test.tsx"
-  //     ],
-  //   }
-  // }
 };

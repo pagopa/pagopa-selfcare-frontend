d154967cbd614cc1d6b85d9e90109c5aa1a87f46
fp <f.piacitelli@gmail.com>
2023-09-15 18:17:08 +0200

    Reduce NodeSignInEcForm test duplicated blocks

--- src/pages/dashboard/nodeSignIn/__tests__/NodeSignInECForm.test.tsx
+++ src/pages/dashboard/nodeSignIn/__tests__/NodeSignInECForm.test.tsx
@@ -5,10 +5,52 @@ import { createMemoryHistory } from 'history';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
-import { store } from '../../../../redux/store';
+import { createStore, store } from '../../../../redux/store';
 import NodeSignInECForm from '../NodeSignInECForm';
 import { PortalApi } from '../../../../api/PortalApiClient';
 import { CreditorInstitutionDetailsResource } from '../../../../api/generated/portal/CreditorInstitutionDetailsResource';
+
+const renderApp = (
+  injectedStore?: ReturnType<typeof createStore>,
+  injectedHistory?: ReturnType<typeof createMemoryHistory>,
+  ecNodeData?: CreditorInstitutionDetailsResource
+) => {
+  const store = injectedStore ? injectedStore : createStore();
+  const history = injectedHistory ? injectedHistory : createMemoryHistory();
+  render(
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Router history={history}>
+          <NodeSignInECForm goBack={jest.fn()} ecNodeData={ecNodeData} />
+        </Router>
+      </ThemeProvider>
+    </Provider>
+  );
+  return { store, history };
+};
+
+const setupForm = () => {
+  const address = screen.getByTestId('address-test') as HTMLInputElement;
+  const city = screen.getByTestId('city-test') as HTMLInputElement;
+  const province = screen.getByTestId('province-test') as HTMLSelectElement;
+  const CAP = screen.getByTestId('CAP-test') as HTMLInputElement;
+  const fiscalDomicile = screen.getByTestId('fiscal-domicile-test') as HTMLInputElement;
+
+  fireEvent.change(address, { target: { value: 'Via Calindri 21' } });
+  expect(address.value).toBe('Via Calindri 21');
+
+  fireEvent.change(city, { target: { value: 'Milano' } });
+  expect(city.value).toBe('Milano');
+
+  fireEvent.change(province, { target: { value: 'MI' } });
+  expect(province.value).toBe('MI');
+
+  fireEvent.change(CAP, { target: { value: '11111' } });
+  expect(CAP.value).toBe('11111');
+
+  fireEvent.change(fiscalDomicile, { target: { value: 'Via Calindri 21' } });
+  expect(fiscalDomicile.value).toBe('Via Calindri 21');
+};
 
 beforeEach(() => {
   jest.spyOn(console, 'error').mockImplementation(() => {});
@@ -21,108 +63,27 @@ describe('NodeSignInECForm', (injectedHistory?: ReturnType<typeof createMemoryHi
   const history = injectedHistory ? injectedHistory : createMemoryHistory();
 
   test('Test rendering NodeSignInECForm and Sumbit', async () => {
-    render(
-      <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <Router history={history}>
-            <NodeSignInECForm goBack={jest.fn()} />
-          </Router>
-        </ThemeProvider>
-      </Provider>
-    );
+    renderApp();
 
-    const address = screen.getByTestId('address-test') as HTMLInputElement;
-    const city = screen.getByTestId('city-test') as HTMLInputElement;
-    const province = screen.getByTestId('province-test') as HTMLSelectElement;
-    const CAP = screen.getByTestId('CAP-test') as HTMLInputElement;
-    const fiscalDomicile = screen.getByTestId('fiscal-domicile-test') as HTMLInputElement;
-
-    fireEvent.change(address, { target: { value: 'Via Calindri 21' } });
-    expect(address.value).toBe('Via Calindri 21');
-
-    fireEvent.change(city, { target: { value: 'Milano' } });
-    expect(city.value).toBe('Milano');
-
-    fireEvent.change(province, { target: { value: 'MI' } });
-    expect(province.value).toBe('MI');
-
-    fireEvent.change(CAP, { target: { value: '12345' } });
-    expect(CAP.value).toBe('12345');
-
-    fireEvent.change(fiscalDomicile, { target: { value: 'Via Calindri 21' } });
-    expect(fiscalDomicile.value).toBe('Via Calindri 21');
+    setupForm();
 
     const confirmBtn = await screen.findByTestId('continue-button-test');
     fireEvent.click(confirmBtn);
   });
 
   test('Test rendering NodeSignInECForm and Sumbit', async () => {
-    render(
-      <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <Router history={history}>
-            <NodeSignInECForm goBack={jest.fn()} />
-          </Router>
-        </ThemeProvider>
-      </Provider>
-    );
+    renderApp();
 
-    const address = screen.getByTestId('address-test') as HTMLInputElement;
-    const city = screen.getByTestId('city-test') as HTMLInputElement;
-    const province = screen.getByTestId('province-test') as HTMLSelectElement;
-    const CAP = screen.getByTestId('CAP-test') as HTMLInputElement;
-    const fiscalDomicile = screen.getByTestId('fiscal-domicile-test') as HTMLInputElement;
-
-    fireEvent.change(address, { target: { value: 'Via Calindri 21' } });
-    expect(address.value).toBe('Via Calindri 21');
-
-    fireEvent.change(city, { target: { value: 'Milano' } });
-    expect(city.value).toBe('Milano');
-
-    fireEvent.change(province, { target: { value: 'MI' } });
-    expect(province.value).toBe('MI');
-
-    fireEvent.change(CAP, { target: { value: '12345' } });
-    expect(CAP.value).toBe('12345');
-
-    fireEvent.change(fiscalDomicile, { target: { value: 'Via Calindri 21' } });
-    expect(fiscalDomicile.value).toBe('Via Calindri 21');
+    setupForm();
 
     const backBtn = await screen.findByTestId('back-button-test');
     fireEvent.click(backBtn);
   });
 
   test('Test rendering NodeSignInECForm and Sumbit', async () => {
-    render(
-      <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <Router history={history}>
-            <NodeSignInECForm goBack={jest.fn()} />
-          </Router>
-        </ThemeProvider>
-      </Provider>
-    );
+    renderApp();
 
-    const address = screen.getByTestId('address-test') as HTMLInputElement;
-    const city = screen.getByTestId('city-test') as HTMLInputElement;
-    const province = screen.getByTestId('province-test') as HTMLSelectElement;
-    const CAP = screen.getByTestId('CAP-test') as HTMLInputElement;
-    const fiscalDomicile = screen.getByTestId('fiscal-domicile-test') as HTMLInputElement;
-
-    fireEvent.change(address, { target: { value: 'Via Calindri 21' } });
-    expect(address.value).toBe('Via Calindri 21');
-
-    fireEvent.change(city, { target: { value: 'Milano' } });
-    expect(city.value).toBe('Milano');
-
-    fireEvent.change(province, { target: { value: 'MI' } });
-    expect(province.value).toBe('MI');
-
-    fireEvent.change(CAP, { target: { value: '11111' } });
-    expect(CAP.value).toBe('11111');
-
-    fireEvent.change(fiscalDomicile, { target: { value: 'Via Calindri 21' } });
-    expect(fiscalDomicile.value).toBe('Via Calindri 21');
+    setupForm();
 
     const confirmBtn = await screen.findByTestId('continue-button-test');
     fireEvent.click(confirmBtn);

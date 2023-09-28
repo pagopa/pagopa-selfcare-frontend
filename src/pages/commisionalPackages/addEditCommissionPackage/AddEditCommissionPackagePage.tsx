1dcec8358cad387426e0f22321807a50207f7fa8
Gabriele <fmaiocch@emeal.nttdata.com>
2023-09-28 16:22:07 +0200

    updated test

--- src/pages/commisionalPackages/addEditCommissionPackage/AddEditCommissionPackagePage.tsx
+++ src/pages/commisionalPackages/addEditCommissionPackage/AddEditCommissionPackagePage.tsx
@@ -2,7 +2,7 @@ import { ArrowBack } from '@mui/icons-material';
 import { Grid, Stack, Breadcrumbs, Typography } from '@mui/material';
 import { ButtonNaked } from '@pagopa/mui-italia';
 import { useTranslation } from 'react-i18next';
-import { TitleBox, useLoading } from '@pagopa/selfcare-common-frontend';
+import { TitleBox, useErrorDispatcher, useLoading } from '@pagopa/selfcare-common-frontend';
 import { useHistory, useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import ROUTES from '../../../routes';
@@ -36,24 +36,41 @@ const commissionPackageDetailsEmpty: CommissionPackageOnCreation = {
 const AddEditCommissionPackagePage = () => {
   const { t } = useTranslation();
   const history = useHistory();
+  const addError = useErrorDispatcher();
   const selectedParty = useAppSelector(partiesSelectors.selectPartySelected);
   const setLoading = useLoading(LOADING_TASK_COMMISSION_PACKAGE_DETAIL);
-  //   const addError = useErrorDispatcher();
   const { nameId, actionId } = useParams<{ nameId: string; actionId: string }>();
   const formAction = actionId ?? FormAction.Create;
   const goBack = () => history.push(ROUTES.COMMISSION_PACKAGES);
   const [commissionPackageDetails, setCommissionPackageDetails] =
     useState<CommissionPackageOnCreation>(commissionPackageDetailsEmpty);
 
+  const getDetails = async () => {
+    setLoading(true);
+    try {
+      await getCommissionPackageDetails(nameId);
+    } catch (reason) {
+      addError({
+        id: 'GET_COMMISSION_PACKAGE_DETAILS',
+        blocking: false,
+        error: reason as Error,
+        techDescription: `An error occurred while getting commission package details`,
+        toNotify: true,
+        displayableTitle: t('commissionPackagesPage.addEditCommissionPackage.error.errorTitle'),
+        displayableDescription: t(
+          'commissionPackagesPage.addEditCommissionPackage.error.commissionPackageDetailsErrorMessageDesc'
+        ),
+        component: 'Toast',
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
     if (formAction !== FormAction.Create) {
-      setLoading(true);
-      getCommissionPackageDetails(nameId)
-        .then((res) => {
-          setCommissionPackageDetails(res);
-        })
-        .catch((reason) => console.error(reason))
-        .finally(() => setLoading(false));
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      getDetails();
     }
   }, []);
 

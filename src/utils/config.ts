ae189435dcc5769abd650b6df64c8956597227ad
Jacopo <jacopo.carlini@gmail.com>
2023-12-01 11:45:02 +0100

    config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,31 +1,44 @@
 import * as env from "env-var";
 
 export enum Type {
-    string ,
+    string,
     boolean,
     int
 }
+
 export function getConfig(param: string, options?: { default?: any; type?: Type; required?: boolean }) {
-    
+
     // eslint-disable-next-line no-underscore-dangle
 
+    const actual_environment = env.get('REACT_APP_ENV').required().asString();
     /*eslint-disable */
-    if (!("_env_" in window) || !(window as any)._env_[param]) {
-        let value = env.get(param)
-            .required(options?.required ?? false);
-        if (options?.default){
-            value = value.default(options.default);
-        }
-        if (Type.int === options?.type){
-            return value.asInt()
-        }
-        if (Type.boolean === options?.type){
-            return value.asBool()
-        }
-        return value.asString();
-
+    if (actual_environment === "prod" || (!("_env_" in window) || !(window as any)._env_[param])) {
+        return getVarFromEnvironment(param, options);
     }
-    // eslint-disable-next-line: no-any
-    return (window as any)._env_[param].toString();
+    return getVarFromWindow(param, options);
 }
 
+function getVarFromEnvironment(param: string, options?: { default?: any; type?: Type; required?: boolean }) {
+    let value = env.get(param)
+        .required(options?.required ?? false);
+    if (options?.default) {
+        value = value.default(options.default);
+    }
+    if (Type.int === options?.type) {
+        return value.asInt()
+    }
+    if (Type.boolean === options?.type) {
+        return value.asBool()
+    }
+    return value.asString();
+}
+
+function getVarFromWindow(param: string, options?: { default?: any; type?: Type; required?: boolean }) {
+    if (Type.int === options?.type) {
+        return +((window as any)._env_[param])
+    }
+    if (Type.boolean === options?.type) {
+        return (window as any)._env_[param] === "true"
+    }
+    return (window as any)._env_[param].toString();
+}

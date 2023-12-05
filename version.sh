b5df203f7f1dfc6a285c35f5379be48a1383dbc1
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-12-05 17:19:08 +0100

    core: removed comments

--- version.sh
+++ version.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 echo "{" > ./public/version.json
-# appname=$(printf '\t"name": "%s",\n' "${npm_package_name}")
-# appversion=$(printf '\t"version": "%s",\n' "${npm_package_version}")
 appenv="local"
 for react_var in $(env | grep -i REACT_APP_ENV); do
     appenv=$(printf '%s\n' "$react_var" | sed -e 's/^[^=]*=//')

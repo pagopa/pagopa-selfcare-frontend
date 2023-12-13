913d9cdfb0ee6c9bc3a692971a08558066e5b515
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2023-12-13 17:39:26 +0100

    [PASECL-641] fix: removed blocks on IBAN update if validity date is after today date

--- src/utils/common-utils.ts
+++ src/utils/common-utils.ts
@@ -2,3 +2,6 @@ import { Iban } from "../api/generated/portal/Iban";
 
 export const isIbanValid = (iban?: Iban) : boolean =>
     iban && iban.due_date && (new Date(iban.due_date) > new Date()) || false;
+
+export const isIbanValidityDateEditable = (iban?: Iban) : boolean =>
+    iban && iban.validity_date && (new Date(iban.validity_date) > new Date()) || false;

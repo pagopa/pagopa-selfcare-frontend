5df59acfeb909fccbe2b0f6aa59b09d013a5f745
Jacopo <jacopo.carlini@gmail.com>
2024-01-05 16:00:31 +0100

    [PASELC-643] apikey FdR

--- src/model/ApiKey.tsx
+++ src/model/ApiKey.tsx
@@ -1,33 +1,37 @@
 export type ProductKeys = {
-  id: string;
-  displayName: string;
-  primaryKey: string;
-  secondaryKey: string;
+    id: string;
+    displayName: string;
+    primaryKey: string;
+    secondaryKey: string;
 };
 
 export type AvailableProductKeys = {
-  id: string;
-  title: string;
-  disabled: boolean;
+    id: string;
+    title: string;
+    disabled: boolean;
 };
 
 export type ConfiguredProductKeys = {
-  id: string;
-  key: string;
+    id: string;
+    key: string;
 };
 
 export const NODOAUTH = 'Connessione con nodo';
-export const GPD = 'Integrazione Asincrona';
-export const GPD_REP = 'Gestione Flussi di Rendicontazione';
-export const BIZ = 'Recupero Ricevuta';
+export const GPD = 'GPD - Integrazione Asincrona';
+export const GPD_REP = 'GPD - Gestione Flussi di Rendicontazione';
+export const BIZ = 'BIZ - Recupero Ricevuta';
+export const FDR_ORG = 'FdR - Flussi di Rendicontazione [ORG]';
+export const FDR_PSP = 'FdR - Flussi di Rendicontazione [PSP]';
 
 export const API_KEY_PSP_PRODUCTS: Array<ConfiguredProductKeys> = [
-  { id: 'NODOAUTH', key: NODOAUTH },
+    {id: 'NODOAUTH', key: NODOAUTH},
+    {id: 'FDR_PSP', key: FDR_PSP},
 ];
 
 export const API_KEY_PRODUCTS: Array<ConfiguredProductKeys> = [
-  { id: 'NODOAUTH', key: NODOAUTH },
-  { id: 'GPD_REP', key: GPD_REP },
-  { id: 'GPD', key: GPD },
-  { id: 'BIZ', key: BIZ },
+    {id: 'NODOAUTH', key: NODOAUTH},
+    {id: 'GPD_REP', key: GPD_REP},
+    {id: 'GPD', key: GPD},
+    {id: 'BIZ', key: BIZ},
+    {id: 'FDR_ORG', key: FDR_ORG},
 ];
--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,16 +1,14 @@
-import { BackofficeApi } from '../api/BackofficeClient';
-import { ProductModel, productResource2Product } from '../model/Product';
+import {BackofficeApi} from '../api/BackofficeClient';
+import {ProductModel, productResource2Product} from '../model/Product';
 import {ENV} from "../utils/env";
-import { mockedPartyProducts } from './__mocks__/productService';
+import {mockedPartyProducts} from './__mocks__/productService';
 
 export const fetchProducts = (partyId: string): Promise<Array<ProductModel>> => {
-  /* istanbul ignore if */
-  if (ENV.MOCK.BACKOFFICE) {
-    return new Promise((resolve) => resolve(mockedPartyProducts));
-  } else {
-
-
-    return BackofficeApi.getProducts(partyId).then((productResources) =>productResources ? productResources.map(productResource2Product) : []
-    );
-  }
+    /* istanbul ignore if */
+    if (ENV.MOCK.BACKOFFICE) {
+        return new Promise((resolve) => resolve(mockedPartyProducts));
+    } else {
+        return BackofficeApi.getProducts(partyId).then((productResources) => productResources ? productResources.map(productResource2Product) : []
+        );
+    }
 };

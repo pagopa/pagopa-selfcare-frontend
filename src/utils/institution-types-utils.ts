2a8dae0088e2c3d7a7c0bd16b10772ee570b5e56
Jacopo <jacopo.carlini@gmail.com>
2024-02-13 15:22:29 +0100

    [VAS-502] lint

--- src/utils/institution-types-utils.ts
+++ src/utils/institution-types-utils.ts
@@ -1,3 +1,1 @@
-export const checkInstitutionTypes = (institutionType: string, validInstitutionTypes: Array<string>) => {
-     return institutionType && validInstitutionTypes.includes(institutionType);
-}
+export const checkInstitutionTypes = (institutionType: string, validInstitutionTypes: Array<string>) => institutionType && validInstitutionTypes.includes(institutionType);

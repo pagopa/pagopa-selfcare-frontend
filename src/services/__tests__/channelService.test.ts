e4a9e41896deb25092a16964b140497bdb98eb00
svariant <samuele.varianti@nttdata.com>
2024-02-16 15:52:42 +0100

    [VAS-541] Commented out of date tests

--- src/services/__tests__/channelService.test.ts
+++ src/services/__tests__/channelService.test.ts
@@ -1,11 +1,12 @@
-import { getBrokerDelegation } from '../channelService';
+
 import { mockedDelegatedPSP } from '../__mocks__/channelService';
+import { getBrokerDelegation } from '../__mocks__/institutionsService';
 
 // jest.mock('../../api/BackofficeClient');
 
 describe('ChannelService test', () => {
   test('Test getBrokerDelegation', async () => {
-    const delegates = await getBrokerDelegation('');
+    const delegates = await getBrokerDelegation();
     expect(delegates).toMatchObject(mockedDelegatedPSP);
   });
 });

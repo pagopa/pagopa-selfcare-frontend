fef655d11835859a5e2c8ef29acfa596951f6c74
svariant <samuele.varianti@nttdata.com>
2024-02-22 14:30:52 +0100

    [VAS-741] ApiKeys components snapshot tests

---
+++ src/pages/apiKeys/__tests__/AddApiKeyPage.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from '../../../redux/store';
+import { render } from '@testing-library/react';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { theme } from '@pagopa/mui-italia';
+import { ThemeProvider } from '@mui/system';
+import routes from '../../../routes';
+import AddApiKeyPage from '../AddApiKeyPage';
+
+//SNAPSHOT TESTING
+it('renders correctly', () => {
+  const tree = render(
+    <Provider store={store}>
+      <MemoryRouter initialEntries={[routes.APIKEYS]}>
+        <Route path={routes.APIKEYS}>
+          <ThemeProvider theme={theme}>
+            <AddApiKeyPage />
+          </ThemeProvider>
+        </Route>
+      </MemoryRouter>
+    </Provider>
+  );
+  expect(tree).toMatchSnapshot();
+});
---
+++ src/pages/apiKeys/__tests__/ApiKeysCard.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from '../../../redux/store';
+import { render } from '@testing-library/react';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { theme } from '@pagopa/mui-italia';
+import { ThemeProvider } from '@mui/system';
+import routes from '../../../routes';
+import ApiKeysCard from '../ApiKeysCard';
+import { getInstitutionApiKeys } from '../../../services/__mocks__/apiKeyService';
+import { ProductKeys } from '../../../model/ApiKey';
+
+//SNAPSHOT TESTING
+it('renders correctly', () => {
+  getInstitutionApiKeys('').then((data) => {
+    const tree = render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={[routes.APIKEYS]}>
+          <Route path={routes.APIKEYS}>
+            <ThemeProvider theme={theme}>
+              <ApiKeysCard apiKey={data[0]} />
+            </ThemeProvider>
+          </Route>
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});

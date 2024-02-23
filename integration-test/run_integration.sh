7221d0e58036b2370d2becc33e39e4704c1caa30
Alessio Cialini <alessio.cialini@emeal.nttdata.com>
2024-02-23 12:46:05 +0100

    [VAS-502] feat: Introduced test for associate channel

--- integration-test/run_integration.sh
+++ integration-test/run_integration.sh
@@ -2,6 +2,8 @@ rm -rf ./user-data/
 node ./00_login.js
 node ./01_iban_add.js
 node ./01_iban_delete.js
-#node ./02_create_station.js
+node ./02_create_station.js
 node ./03_export_ibans.js
 node ./04_associate_station.js
+node ./switch_to_psp_demo_direct.js
+node ./06_associate_channel.js

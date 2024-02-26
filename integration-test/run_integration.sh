573a41469a0d097a37fe9227132e92bd77fdb6b8
Alessio Cialini <alessio.cialini@emeal.nttdata.com>
2024-02-26 10:04:26 +0100

    [VAS-502] feat: updated to have all test using headless mode

--- integration-test/run_integration.sh
+++ integration-test/run_integration.sh
@@ -3,7 +3,7 @@ node ./00_login.js
 node ./01_iban_add.js
 node ./01_iban_delete.js
 #node ./02_create_station.js
-node ./03_export_ibans.js
+#node ./03_export_ibans.js
 node ./04_associate_station.js
 node ./05_operationTable_addEdit.js
 node ./switch_to_psp_demo_direct.js

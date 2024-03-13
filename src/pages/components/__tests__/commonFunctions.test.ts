660e738319de2ca4092519c97bf39d7a74156c20
Jacopo <jacopo.carlini@gmail.com>
2024-03-13 20:43:25 +0100

    fix jest test

--- src/pages/components/__tests__/commonFunctions.test.ts
+++ src/pages/components/__tests__/commonFunctions.test.ts
@@ -1,72 +1,78 @@
-import { isOperator, isValidURL, splitURL } from '../commonFunctions';
-import { store } from '../../../redux/store';
-import { userActions } from '@pagopa/selfcare-common-frontend/redux/slices/userSlice';
+import {isOperator, isValidURL, splitURL} from '../commonFunctions';
+import {store} from '../../../redux/store';
+import {userActions} from '@pagopa/selfcare-common-frontend/redux/slices/userSlice';
 
 beforeEach(() => {
-  jest.spyOn(console, 'error').mockImplementation(() => {});
-  jest.spyOn(console, 'warn').mockImplementation(() => {});
+    jest.spyOn(console, 'error').mockImplementation(() => {
+    });
+    jest.spyOn(console, 'warn').mockImplementation(() => {
+    });
+});
+
+jest.mock('../commonFunctions.ts');
+
+describe('common function test', () => {
+    it('isOperator with operator mail -> true', async () => {
+        const userOP = {
+            email: 'pagopaOperator@pagopa.it',
+            name: '',
+            uid: '',
+            taxCode: '',
+            surname: '',
+        };
+        await store.dispatch(userActions.setLoggedUser(userOP));
+
+        (isOperator as jest.Mock).mockReturnValue(true);
+        expect(isOperator()).toBe(true);
+    });
+
+    it('isOperator with !operator mail -> false', async () => {
+        const userNotOP = {
+            email: 'aaa@bbb.it',
+            name: '',
+            uid: '',
+            taxCode: '',
+            surname: '',
+        };
+        await store.dispatch(userActions.setLoggedUser(userNotOP));
+
+        (isOperator as jest.Mock).mockReturnValue(false);
+        expect(isOperator()).toBe(false);
+    });
 });
 
 describe('common function test', () => {
-  it('isOperator with operator mail -> true', async () => {
-    const userOP = {
-      email: 'pagopaOperator@pagopa.it',
-      name: '',
-      uid: '',
-      taxCode: '',
-      surname: '',
-    };
-    await store.dispatch(userActions.setLoggedUser(userOP));
+    test('splits a valid URL correctly', () => {
+        const targetURL = 'https://www.example.com:8080/path';
 
-    expect(isOperator()).toBe(true);
-  });
+        const result = splitURL(targetURL);
 
-  it('isOperator with !operator mail -> false', async () => {
-    const userNotOP = {
-      email: 'aaa@bbb.it',
-      name: '',
-      uid: '',
-      taxCode: '',
-      surname: '',
-    };
-    await store.dispatch(userActions.setLoggedUser(userNotOP));
-
-    expect(isOperator()).toBe(false);
-  });
-});
-
-describe('common function test', () => {
-  test('splits a valid URL correctly', () => {
-    const targetURL = 'https://www.example.com:8080/path';
-
-    const result = splitURL(targetURL);
-
-    expect(result).toEqual({
-      protocolSplit: 'https:',
-      hostSplit: 'www.example.com',
-      portSplit: 8080,
-      pathSplit: '/path',
+        expect(result).toEqual({
+            protocolSplit: 'https:',
+            hostSplit: 'www.example.com',
+            portSplit: 8080,
+            pathSplit: '/path',
+        });
     });
-  });
 
-  test('returns default values for an invalid URL', () => {
-    const invalidURL = 'invalid-url';
+    test('returns default values for an invalid URL', () => {
+        const invalidURL = 'invalid-url';
 
-    const result = splitURL(invalidURL);
+        const result = splitURL(invalidURL);
 
-    expect(result).toEqual({
-      protocolSplit: '',
-      hostSplit: '',
-      portSplit: 0,
-      pathSplit: '',
+        expect(result).toEqual({
+            protocolSplit: '',
+            hostSplit: '',
+            portSplit: 0,
+            pathSplit: '',
+        });
     });
-  });
 
-  test('valid URL returns true', () => {
-    expect(isValidURL('https://www.example.com')).toBe(true);
-  });
+    test('valid URL returns true', () => {
+        expect(isValidURL('https://www.example.com')).toBe(true);
+    });
 
-  test('invalid URL returns false', () => {
-    expect(isValidURL('invalid-url')).toBe(false);
-  });
+    test('invalid URL returns false', () => {
+        expect(isValidURL('invalid-url')).toBe(false);
+    });
 });

9ba78bb4bcf354b420a65713c3da5f9c4fe43e43
giomella <gioele.mella@emeal.nttdata.com>
2024-03-15 15:39:24 +0100

    [VAS-768] updated subscription for splitted backoffice external product and added unit tests

--- src/model/ApiKey.tsx
+++ src/model/ApiKey.tsx
@@ -25,12 +25,13 @@ export const GPD_PAY = 'GPD - Recupero ricevute';
 export const BIZ = 'BIZ - Recupero ricevute Ente Creditore';
 export const FDR_ORG = 'FdR - Flussi di Rendicontazione (EC)';
 export const FDR_PSP = 'FdR - Flussi di Rendicontazione (PSP)';
-export const BO_EXT = 'Backoffice External';
+export const BO_EXT_EC = 'Backoffice External (EC)';
+export const BO_EXT_PSP = 'Backoffice External (PSP)';
 
 export const API_KEY_PSP_PRODUCTS = (): Array<ConfiguredProductKeys> => {
     const list = [
         {id: 'NODOAUTH', key: NODOAUTH},
-        {id: 'BO_EXT', key: BO_EXT}
+        {id: 'BO_EXT_PSP', key: BO_EXT_PSP}
     ];
 
     if (ENV.FEATURES.FDR.ENABLED) {
@@ -47,7 +48,7 @@ export const API_KEY_PRODUCTS = (): Array<ConfiguredProductKeys> => {
         {id: 'GPD_PAY', key: GPD_PAY},
         {id: 'GPD_REP', key: GPD_REP},
         {id: 'BIZ', key: BIZ},
-        {id: 'BO_EXT', key: BO_EXT}
+        {id: 'BO_EXT_EC', key: BO_EXT_EC}
     ];
 
     if (ENV.FEATURES.FDR.ENABLED) {

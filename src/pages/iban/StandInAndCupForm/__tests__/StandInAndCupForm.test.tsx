e5cc88d7582292a4db16ce621596ec5f9f4b20d1
Jacopo Carlini <jacopo.carlini@gmail.com>
2024-04-03 11:52:10 +0200

    fix currency format global bundle (#427)
    * fix currency format global bundle
    
    * fix test
    
    * fix
    
    * fix
    
    * rollback
    
    * Bump to version 1.18.24-1-hotfix-bundle-currency-format [skip ci]
    
    * fix test
    
    * fix unit test
    
    ---------
    
    Co-authored-by: pagopa-github-bot <github-bot@pagopa.it>
    Co-authored-by: giomella <gioele.mella@emeal.nttdata.com>

--- src/pages/iban/StandInAndCupForm/__tests__/StandInAndCupForm.test.tsx
+++ src/pages/iban/StandInAndCupForm/__tests__/StandInAndCupForm.test.tsx
@@ -1,182 +1,175 @@
 import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import StandInAndCupForm from '../StandInAndCupForm';
-import { ThemeProvider } from '@mui/system';
-import { theme } from '@pagopa/mui-italia';
-import { Provider } from 'react-redux';
-import { MemoryRouter, Route } from 'react-router-dom';
-import { store } from '../../../../redux/store';
-import { ibanList } from '../../../../services/__mocks__/ibanService';
-import { Ibans } from '../../../../api/generated/portal/Ibans';
+import {ThemeProvider} from '@mui/system';
+import {theme} from '@pagopa/mui-italia';
+import {Provider} from 'react-redux';
+import {MemoryRouter, Route} from 'react-router-dom';
+import {store} from '../../../../redux/store';
+import {ibanList} from '../../../../services/__mocks__/ibanService';
+import {Ibans} from '../../../../api/generated/portal/Ibans';
 
 beforeEach(() => {
-  jest.spyOn(console, 'error').mockImplementation(() => {});
-  jest.spyOn(console, 'warn').mockImplementation(() => {});
+    jest.spyOn(console, 'error').mockImplementation(() => {
+    });
+    jest.spyOn(console, 'warn').mockImplementation(() => {
+    });
 });
 
 const TestStandInAndCupForm = ({
-  ibanList
-}: {
-  ibanList: Ibans
+                                   ibanList
+                               }: {
+    ibanList: Ibans
 }) => {
-  return (
-    <Provider store={store}>
-      <MemoryRouter initialEntries={[`/iban`]}>
-        <Route path="/iban">
-          <ThemeProvider theme={theme}>
-            <StandInAndCupForm ibanList={ibanList} error={false} loading={false} />
-          </ThemeProvider>
-        </Route>
-      </MemoryRouter>
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <MemoryRouter initialEntries={[`/iban`]}>
+                <Route path="/iban">
+                    <ThemeProvider theme={theme}>
+                        <StandInAndCupForm ibanList={ibanList} error={false} loading={false}/>
+                    </ThemeProvider>
+                </Route>
+            </MemoryRouter>
+        </Provider>
+    );
 };
 
 describe('StandInAndCupForm', () => {
-  it('Test render StandInAndCupForm', () => {
-    render(<TestStandInAndCupForm ibanList={ibanList} />);
-  });
+    it('Test render StandInAndCupForm', () => {
+        render(<TestStandInAndCupForm ibanList={ibanList}/>);
+    });
 
-  it('Input test with manageButton false and no cup and standIn selected, update with different iban', async () => {
-    const ibanListMocked: Ibans = {
-      ibans_enhanced: [
-        {
-          iban: 'IT60X0542811101000000123456',
-          publication_date: '2024-06-01T23:59:59.999Z',
-          validity_date: '2024-04-01T13:49:19.897Z',
-          due_date: '2024-04-01T13:49:19.897Z',
-          ci_owner: 'RSSMRA98H27F205Q',
-          labels: [],
-          description: 'Tassa di concorso - servizio tesoreria comunale',
-          is_active: true,
-        },
-        {
-          iban: 'IT60X0542811101000000123457',
-          publication_date: '2024-06-01T23:59:59.999Z',
-          validity_date: '2024-04-01T13:49:19.897Z',
-          due_date: '2024-04-01T13:49:19.897Z',
-          ci_owner: 'RSSMRA98H27F205Q',
-          labels: [],
-          description: 'Tassa di concorso - servizio tesoreria comunale',
-          is_active: true,
-        },
-      ],
-    };
+    it('Input test with manageButton false and no cup and standIn selected, update with different iban', async () => {
+        const ibanListMocked: Ibans = {
+            ibans_enhanced: [
+                {
+                    iban: 'IT60X0542811101000000123456',
+                    publication_date: '2024-06-01T23:59:59.999Z',
+                    validity_date: '2024-04-01T13:49:19.897Z',
+                    due_date: '2024-04-01T13:49:19.897Z',
+                    ci_owner: 'RSSMRA98H27F205Q',
+                    labels: [],
+                    description: 'Tassa di concorso - servizio tesoreria comunale',
+                    is_active: true,
+                },
+                {
+                    iban: 'IT60X0542811101000000123457',
+                    publication_date: '2024-06-01T23:59:59.999Z',
+                    validity_date: '2024-04-01T13:49:19.897Z',
+                    due_date: '2024-04-01T13:49:19.897Z',
+                    ci_owner: 'RSSMRA98H27F205Q',
+                    labels: [],
+                    description: 'Tassa di concorso - servizio tesoreria comunale',
+                    is_active: true,
+                },
+            ],
+        };
 
-    render(<TestStandInAndCupForm ibanList={ibanListMocked} />);
+        render(<TestStandInAndCupForm ibanList={ibanListMocked}/>);
 
-    const manageButton = screen.getByTestId('iban-manage-btn');
-    fireEvent.click(manageButton);
+        const manageButton = screen.getByTestId('iban-manage-btn');
+        fireEvent.click(manageButton);
 
-    const manageChip = screen.getByText('ibanPage.updateInProgress');
-    expect(manageChip).toBeInTheDocument();
+        const manageChip = screen.getByText('ibanPage.updateInProgress');
+        expect(manageChip).toBeInTheDocument();
 
-    const standInIbanSelect = screen.getByTestId('stand-in-test');
-    fireEvent.click(standInIbanSelect);
+        const standInIbanSelect = screen.getByTestId('stand-in-test');
+        fireEvent.click(standInIbanSelect);
 
-    const cupIbanSelect = screen.getByTestId('cup-test');
+        const cupIbanSelect = screen.getByTestId('cup-test');
 
-    fireEvent.change(standInIbanSelect, { target: { value: 'IT60X0542811101000000123456' } });
-    fireEvent.change(cupIbanSelect, { target: { value: 'IT60X0542811101000000123457' } });
+        fireEvent.change(standInIbanSelect, {target: {value: 'IT60X0542811101000000123456'}});
+        fireEvent.change(cupIbanSelect, {target: {value: 'IT60X0542811101000000123457'}});
 
-    const uploadIbans = screen.getByTestId('upload-iban-test');
-    fireEvent.click(uploadIbans);
+        const uploadIbans = screen.getByTestId('upload-iban-test');
+        fireEvent.click(uploadIbans);
 
-    const confirmBtn = await screen.findByTestId('confirm-button-test');
-    fireEvent.click(confirmBtn);
+        const confirmBtn = await screen.findByTestId('confirm-button-test');
+        fireEvent.click(confirmBtn);
 
-    fireEvent.submit(confirmBtn);
+        fireEvent.submit(confirmBtn);
+    });
 
-    const backBtn = screen.getByTestId('back-button-test');
-    fireEvent.click(backBtn);
-  });
+    it('Input test with manageButton false and with cup and standIn selected, update with different iban', async () => {
+        render(<TestStandInAndCupForm ibanList={ibanList}/>);
 
-  it('Input test with manageButton false and with cup and standIn selected, update with different iban', async () => {
-    render(<TestStandInAndCupForm ibanList={ibanList} />);
+        const manageButton = screen.getByTestId('iban-manage-btn');
+        fireEvent.click(manageButton);
 
-    const manageButton = screen.getByTestId('iban-manage-btn');
-    fireEvent.click(manageButton);
+        const manageChip = screen.getByText('ibanPage.updateInProgress');
+        expect(manageChip).toBeInTheDocument();
 
-    const manageChip = screen.getByText('ibanPage.updateInProgress');
-    expect(manageChip).toBeInTheDocument();
+        const standInIbanSelect = screen.getByTestId('stand-in-test');
+        fireEvent.click(standInIbanSelect);
 
-    const standInIbanSelect = screen.getByTestId('stand-in-test');
-    fireEvent.click(standInIbanSelect);
+        const cupIbanSelect = screen.getByTestId('cup-test');
 
-    const cupIbanSelect = screen.getByTestId('cup-test');
+        fireEvent.change(standInIbanSelect, {target: {value: 'IT99C0222211111000000000003'}});
+        fireEvent.change(cupIbanSelect, {target: {value: 'IT99C0222211111000000000004'}});
 
-    fireEvent.change(standInIbanSelect, { target: { value: 'IT99C0222211111000000000003' } });
-    fireEvent.change(cupIbanSelect, { target: { value: 'IT99C0222211111000000000004' } });
+        const uploadIbans = screen.getByTestId('upload-iban-test');
+        fireEvent.click(uploadIbans);
 
-    const uploadIbans = screen.getByTestId('upload-iban-test');
-    fireEvent.click(uploadIbans);
+        const confirmBtn = await screen.findByTestId('confirm-button-test');
+        fireEvent.click(confirmBtn);
 
-    const confirmBtn = await screen.findByTestId('confirm-button-test');
-    fireEvent.click(confirmBtn);
+        fireEvent.submit(confirmBtn);
+    });
 
-    fireEvent.submit(confirmBtn);
+    it('Input test with manageButton false and with cup and standIn selected, update with same iban', async () => {
+        render(<TestStandInAndCupForm ibanList={ibanList}/>);
 
-    const backBtn = screen.getByTestId('back-button-test');
-    fireEvent.click(backBtn);
-  });
-  
-  it('Input test with manageButton false and with cup and standIn selected, update with same iban', async () => {
-    render(<TestStandInAndCupForm ibanList={ibanList} />);
+        const manageButton = screen.getByTestId('iban-manage-btn');
+        fireEvent.click(manageButton);
 
-    const manageButton = screen.getByTestId('iban-manage-btn');
-    fireEvent.click(manageButton);
+        const manageChip = screen.getByText('ibanPage.updateInProgress');
+        expect(manageChip).toBeInTheDocument();
 
-    const manageChip = screen.getByText('ibanPage.updateInProgress');
-    expect(manageChip).toBeInTheDocument();
+        const standInIbanSelect = screen.getByTestId('stand-in-test');
+        fireEvent.click(standInIbanSelect);
 
-    const standInIbanSelect = screen.getByTestId('stand-in-test');
-    fireEvent.click(standInIbanSelect);
+        const cupIbanSelect = screen.getByTestId('cup-test');
 
-    const cupIbanSelect = screen.getByTestId('cup-test');
+        fireEvent.change(standInIbanSelect, {target: {value: 'IT99C0222211111000000000003'}});
+        fireEvent.change(cupIbanSelect, {target: {value: 'IT99C0222211111000000000003'}});
 
-    fireEvent.change(standInIbanSelect, { target: { value: 'IT99C0222211111000000000003' } });
-    fireEvent.change(cupIbanSelect, { target: { value: 'IT99C0222211111000000000003' } });
+        const uploadIbans = screen.getByTestId('upload-iban-test');
+        fireEvent.click(uploadIbans);
 
-    const uploadIbans = screen.getByTestId('upload-iban-test');
-    fireEvent.click(uploadIbans);
+        const confirmBtn = await screen.findByTestId('confirm-button-test');
+        fireEvent.click(confirmBtn);
 
-    const confirmBtn = await screen.findByTestId('confirm-button-test');
-    fireEvent.click(confirmBtn);
+        fireEvent.submit(confirmBtn);
+    });
 
-    fireEvent.submit(confirmBtn);
+    it('Input test with manageButton false and no cup and standIn selected', async () => {
+        render(<TestStandInAndCupForm ibanList={ibanList}/>);
 
-    const backBtn = screen.getByTestId('back-button-test');
-    fireEvent.click(backBtn);
-  });
-  
-  it('Input test with manageButton false and no cup and standIn selected', async () => {
-    render(<TestStandInAndCupForm ibanList={ibanList} />);
+        const manageButton = screen.getByTestId('iban-manage-btn');
+        fireEvent.click(manageButton);
 
-    const manageButton = screen.getByTestId('iban-manage-btn');
-    fireEvent.click(manageButton);
+        const manageChip = screen.getByText('ibanPage.updateInProgress');
+        expect(manageChip).toBeInTheDocument();
 
-    const manageChip = screen.getByText('ibanPage.updateInProgress');
-    expect(manageChip).toBeInTheDocument();
+        const standInIbanSelect = screen.getByTestId('stand-in-test');
+        fireEvent.click(standInIbanSelect);
 
-    const standInIbanSelect = screen.getByTestId('stand-in-test');
-    fireEvent.click(standInIbanSelect);
+        const uploadIbans = screen.getByTestId('upload-iban-test');
+        fireEvent.click(uploadIbans);
 
-    const uploadIbans = screen.getByTestId('upload-iban-test');
-    fireEvent.click(uploadIbans);
+        const confirmBtn = await screen.findByTestId('confirm-button-test');
+        fireEvent.click(confirmBtn);
 
-    const confirmBtn = await screen.findByTestId('confirm-button-test');
-    fireEvent.click(confirmBtn);
+        fireEvent.submit(confirmBtn);
+    });
 
-    fireEvent.submit(confirmBtn);
-  });
+    it('Input test with manageButton true', () => {
+        render(<TestStandInAndCupForm ibanList={ibanList}/>);
 
-  it('Input test with manageButton true', () => {
-    render(<TestStandInAndCupForm ibanList={ibanList} />);
+        const ibanStandInValue = screen.getByTestId('iban-standin-with-manage-btn-false');
+        const ibanCupValue = screen.getByTestId('iban-cup-with-manage-btn-false');
 
-    const ibanStandInValue = screen.getByTestId('iban-standin-with-manage-btn-false');
-    const ibanCupValue = screen.getByTestId('iban-cup-with-manage-btn-false');
-
-    expect(ibanStandInValue).toBeInTheDocument();
-    expect(ibanCupValue).toBeInTheDocument();
-  });
+        expect(ibanStandInValue).toBeInTheDocument();
+        expect(ibanCupValue).toBeInTheDocument();
+    });
 });

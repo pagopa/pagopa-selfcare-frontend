d22894351b9da5573cec58228cb7fdae22f34489
svariant <samuele.varianti@nttdata.com>
2024-04-09 11:08:46 +0200

    [VAS-686] Fixed unit tests

--- src/components/ProtectedRoute/__tests__/ProtectedRoute.test.tsx
+++ src/components/ProtectedRoute/__tests__/ProtectedRoute.test.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { queryAllByText, queryByText, render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import { MemoryRouter, Route } from 'react-router-dom';
 import { ProtectedRoute } from '../ProtectedRoute';
 import * as usePermissions from '../../../hooks/usePermissions';
+import * as useFlagValue from '../../../hooks/useFeatureFlags';
+import { store } from '../../../redux/store';
 
 beforeEach(() => {
   jest.spyOn(console, 'error').mockImplementation(() => {});
@@ -13,9 +16,35 @@ describe('ProtectedRoute component', () => {
   jest.mock('../../../hooks/usePermissions');
 
   test('should render children when user has permission', () => {
-    jest
-      .spyOn(usePermissions, 'usePermissions')
-      .mockReturnValue({ hasPermission: (permissionName) => true });
+    jest.spyOn(usePermissions, 'usePermissions').mockReturnValue({
+      hasPermission: (_) => true,
+      isPsp: jest.fn(),
+      isEc: jest.fn(),
+      isPspDirect: jest.fn(),
+    });
+    jest.spyOn(useFlagValue, 'useFlagValue').mockReturnValue(true);
+
+    const { getByText } = render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/some-route']}>
+          <ProtectedRoute permission="node-signin">
+            <div>Child Component</div>
+          </ProtectedRoute>
+        </MemoryRouter>
+      </Provider>
+    );
+
+    const childComponent = getByText('Child Component');
+    expect(childComponent).toBeInTheDocument();
+  });
+  test("should not render children when user hasn't permission", () => {
+    jest.spyOn(usePermissions, 'usePermissions').mockReturnValue({
+      hasPermission: (permissionName) => false,
+      isPsp: jest.fn(),
+      isEc: jest.fn(),
+      isPspDirect: jest.fn(),
+    });
+    jest.spyOn(useFlagValue, 'useFlagValue').mockReturnValue(true);
 
     const { getByText } = render(
       <MemoryRouter initialEntries={['/some-route']}>
@@ -25,13 +54,18 @@ describe('ProtectedRoute component', () => {
       </MemoryRouter>
     );
 
-    const childComponent = getByText('Child Component');
-    expect(childComponent).toBeInTheDocument();
+    const childComponent = screen.queryByText('Child Component');
+    expect(childComponent).toBeNull();
   });
-  test("should not render children when user hasn't permission", () => {
-    jest
-      .spyOn(usePermissions, 'usePermissions')
-      .mockReturnValue({ hasPermission: (permissionName) => false });
+
+  test("should not render children when feature flag false", () => {
+    jest.spyOn(usePermissions, 'usePermissions').mockReturnValue({
+      hasPermission: (permissionName) => true,
+      isPsp: jest.fn(),
+      isEc: jest.fn(),
+      isPspDirect: jest.fn(),
+    });
+    jest.spyOn(useFlagValue, 'useFlagValue').mockReturnValue(false);
 
     const { getByText } = render(
       <MemoryRouter initialEntries={['/some-route']}>

c0818e9c2020ef2305ee6e8992751511d5cc66e4
giomella <gioele.mella@emeal.nttdata.com>
2024-04-09 14:22:49 +0200

    [VAS-687] fixed page formatting and added missing functionalities

--- src/pages/delegations/detail/DelegationDetailPaymentContacts.tsx
+++ src/pages/delegations/detail/DelegationDetailPaymentContacts.tsx
@@ -32,24 +32,32 @@ export default function DelegationDetailPaymentContacts({
         ?.filter((_, i) => i < 1)
         ?.map((el) => (
           <Box key={`payment-contact-${el.id}`} mt={1} data-testid="payment-contact-column">
-            <Typography variant="body1" color="action.active">
-              {t('delegationDetailPage.paymentContacts.name')}
-            </Typography>
-            <Typography variant="body1" fontWeight={'fontWeightMedium'}>
-              {el?.name}
-            </Typography>
-            <Typography variant="body1" color="action.active">
-              {t('delegationDetailPage.paymentContacts.surname')}
-            </Typography>
-            <Typography variant="body1" fontWeight={'fontWeightMedium'}>
-              {el?.surname}
-            </Typography>
-            <Typography variant="body1" color="action.active">
-              {t('delegationDetailPage.paymentContacts.email')}
-            </Typography>
-            <Typography variant="body1" fontWeight={'fontWeightMedium'}>
-              {el?.email}
-            </Typography>
+            <Box key={`payment-contact-name-${el.id}`} mt={1}>
+              <Typography variant="body1" color="action.active">
+                {t('delegationDetailPage.paymentContacts.name')}
+              </Typography>
+              <Typography variant="body1" fontWeight={'fontWeightMedium'}>
+                {el?.name ?? '-'}
+              </Typography>
+            </Box>
+
+            <Box key={`payment-contact-surname-${el.id}`} mt={1}>
+              <Typography variant="body1" color="action.active">
+                {t('delegationDetailPage.paymentContacts.surname')}
+              </Typography>
+              <Typography variant="body1" fontWeight={'fontWeightMedium'}>
+                {el?.surname ?? '-'}
+              </Typography>
+            </Box>
+
+            <Box key={`payment-contact-email-${el.id}`} mt={1}>
+              <Typography variant="body1" color="action.active">
+                {t('delegationDetailPage.paymentContacts.email')}
+              </Typography>
+              <Typography variant="body1" fontWeight={'fontWeightMedium'}>
+                {el?.email ?? '-'}
+              </Typography>
+            </Box>
           </Box>
         ))}
       {paymentContacts && paymentContacts.length > 1 && (
@@ -66,36 +74,45 @@ export default function DelegationDetailPaymentContacts({
           </ButtonNaked>
           <PaddedDrawer openDrawer={openDrawer} setOpenDrawer={setOpenDrawer}>
             <TitleBox title={t('delegationDetailPage.paymentContacts.title')} variantTitle="h5" />
-            {paymentContacts &&
-              paymentContacts.map((el, index) => (
-                <React.Fragment key={`payment-contact-${el.id}`}>
-                  {index !== 0 && <Divider />}
-                  <Typography variant="overline">{t('delegationDetailPage.paymentContacts.drawerSubtitle')} {index + 1}</Typography>
-                  <Box
-                    mb={1}
-                    data-testid="payment-contact-drawer-column"
-                  >
+            {paymentContacts?.map((el, index) => (
+              <React.Fragment key={`payment-contact-${el.id}`}>
+                <Box mb={4} data-testid="payment-contact-drawer-column">
+
+                  <Box key={`payment-contact-subtitle-${el.id}`} mt={1}>
+                    <Typography variant="overline">
+                      {t('delegationDetailPage.paymentContacts.drawerSubtitle')} {index + 1}
+                    </Typography>
+                  </Box>
+
+                  <Box key={`payment-contact-name-${el.id}`} mt={1}>
                     <Typography variant="body1" color="action.active">
                       {t('delegationDetailPage.paymentContacts.name')}
                     </Typography>
                     <Typography variant="body1" fontWeight={'fontWeightMedium'}>
-                      {el?.name}
+                      {el?.name ?? '-'}
                     </Typography>
+                  </Box>
+
+                  <Box key={`payment-contact-surname-${el.id}`} mt={1}>
                     <Typography variant="body1" color="action.active">
                       {t('delegationDetailPage.paymentContacts.surname')}
                     </Typography>
                     <Typography variant="body1" fontWeight={'fontWeightMedium'}>
-                      {el?.surname}
+                      {el?.surname ?? '-'}
                     </Typography>
+                  </Box>
+
+                  <Box key={`payment-contact-email-${el.id}`} mt={1}>
                     <Typography variant="body1" color="action.active">
                       {t('delegationDetailPage.paymentContacts.email')}
                     </Typography>
                     <Typography variant="body1" fontWeight={'fontWeightMedium'}>
-                      {el?.email}
+                      {el?.email ?? '-'}
                     </Typography>
                   </Box>
-                </React.Fragment>
-              ))}
+                </Box>
+              </React.Fragment>
+            ))}
           </PaddedDrawer>
         </>
       )}

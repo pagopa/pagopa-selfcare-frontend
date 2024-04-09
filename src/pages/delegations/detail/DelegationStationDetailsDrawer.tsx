1b54fef99e2e346ff989d354d3afdf9380275e95
giomella <gioele.mella@emeal.nttdata.com>
2024-04-09 14:22:49 +0200

    [VAS-687] fixed page formatting and added missing functionalities

--- src/pages/delegations/detail/DelegationStationDetailsDrawer.tsx
+++ src/pages/delegations/detail/DelegationStationDetailsDrawer.tsx
@@ -1,86 +1,163 @@
-import { Chip, Grid, Typography } from "@mui/material";
-import { Box } from "@mui/system";
-import { TitleBox } from "@pagopa/selfcare-common-frontend";
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import { Button, Chip, Grid, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import { ButtonNaked } from '@pagopa/mui-italia';
+import { TitleBox } from '@pagopa/selfcare-common-frontend';
 import { TFunction } from 'react-i18next';
-import { CIBrokerStationResource } from "../../../api/generated/portal/CIBrokerStationResource";
-import { PaddedDrawer } from "../../../components/PaddedDrawer";
-import { formatDateToDDMMYYYY } from "../../../utils/common-utils";
+import { generatePath, useHistory } from 'react-router-dom';
+import { CIBrokerStationResource } from '../../../api/generated/portal/CIBrokerStationResource';
+import { PaddedDrawer } from '../../../components/PaddedDrawer';
+import ROUTES from '../../../routes';
+import { formatDateToDDMMYYYY } from '../../../utils/common-utils';
 
 export const DelegationStationDetailsDrawer = ({
-    t,
-    setDrawerValue,
-    drawerValue
-  }: {
-    t: TFunction<'translation', undefined>;
-    setDrawerValue: (openDrawer: CIBrokerStationResource) => void;
-    drawerValue: CIBrokerStationResource;
-  }) => (
-        <PaddedDrawer openDrawer={drawerValue?.station_code !== undefined} setOpenDrawer={setDrawerValue}>
-          <TitleBox title={t('delegationDetailPage.stationDetail.title')} variantTitle="h5" />
-          <Box
-            key={`station-detail-${drawerValue.station_code}`}
-            mb={1}
-            data-testid="station-detail-drawer-column"
+  t,
+  setDrawerValue,
+  drawerValue,
+  setShowDisassociateStationModal,
+}: {
+  t: TFunction<'translation', undefined>;
+  setDrawerValue: (openDrawer: CIBrokerStationResource) => void;
+  drawerValue: CIBrokerStationResource;
+  setShowDisassociateStationModal: (openModal: string) => void;
+}) => {
+  const history = useHistory();
+
+  return (
+    <PaddedDrawer
+      openDrawer={drawerValue?.station_code !== undefined}
+      setOpenDrawer={setDrawerValue}
+    >
+      <TitleBox title={t('delegationDetailPage.stationDetail.title')} variantTitle="h5" />
+      <Box
+        key={`station-detail-${drawerValue.station_code}`}
+        mb={1}
+        data-testid="station-detail-drawer-column"
+      >
+        <Box mt={3}>
+          <Typography variant="overline">
+            {t('delegationDetailPage.stationDetail.registry')}
+          </Typography>
+        </Box>
+
+        <Box mt={1}>
+          <Typography variant="body1" color="action.active">
+            {t('delegationDetailPage.stationDetail.stationCode')}
+          </Typography>
+          <Typography variant="body1" fontWeight={'fontWeightMedium'}>
+            {drawerValue?.station_code ?? '-'}
+          </Typography>
+        </Box>
+
+        <Box mt={1}>
+          <Typography variant="body1" color="action.active">
+            {t('delegationDetailPage.stationDetail.activationDate')}
+          </Typography>
+          <Typography variant="body1" fontWeight={'fontWeightMedium'}>
+            {drawerValue?.activation_date
+              ? formatDateToDDMMYYYY(drawerValue?.activation_date)
+              : '-'}
+          </Typography>
+        </Box>
+
+        <Box mt={1}>
+          <Typography variant="body1" color="action.active">
+            {t('delegationDetailPage.stationDetail.auxDigit')}
+          </Typography>
+          <Typography variant="body1" fontWeight={'fontWeightMedium'}>
+            {drawerValue?.aux_digit ?? 3}
+          </Typography>
+        </Box>
+
+        <Box mt={1}>
+          <Typography variant="body1" color="action.active">
+            {t('delegationDetailPage.stationDetail.segregationCode')}
+          </Typography>
+          <Typography variant="body1" fontWeight={'fontWeightMedium'}>
+            {drawerValue?.segregation_code ?? '-'}
+          </Typography>
+        </Box>
+
+        <Box mt={1}>
+          <Typography variant="body1" color="action.active">
+            {t('delegationDetailPage.stationDetail.applicationCode')}
+          </Typography>
+          <Typography variant="body1" fontWeight={'fontWeightMedium'}>
+            {drawerValue?.application_code ?? '-'}
+          </Typography>
+        </Box>
+
+        <Box mt={1} mb={3}>
+          <ButtonNaked
+            size="large"
+            component="button"
+            onClick={() =>
+              history.push(
+                generatePath(ROUTES.STATION_DETAIL, { stationId: drawerValue?.station_code })
+              )
+            }
+            sx={{ color: 'primary.main', mt: 'auto', justifyContent: 'start' }}
+            weight="default"
+            data-testid="show-more-payment-contacts-test"
           >
-            <Typography variant="overline">
-              {t('delegationDetailPage.stationDetail.registry')}
-            </Typography>
-            <Typography variant="body1" color="action.active">
-              {t('delegationDetailPage.stationDetail.stationCode')}
-            </Typography>
-            <Typography variant="body1" fontWeight={'fontWeightMedium'}>
-              {drawerValue?.station_code ?? '-'}
-            </Typography>
-            <Typography variant="body1" color="action.active">
-              {t('delegationDetailPage.stationDetail.activationDate')}
-            </Typography>
-            <Typography variant="body1" fontWeight={'fontWeightMedium'}>
-              {drawerValue?.activation_date ?
-              formatDateToDDMMYYYY(drawerValue?.activation_date) : '-'}
-            </Typography>
-            <Typography variant="body1" color="action.active">
-              {t('delegationDetailPage.stationDetail.auxDigit')}
-            </Typography>
-            <Typography variant="body1" fontWeight={'fontWeightMedium'}>
-              {drawerValue?.aux_digit ?? '-'}
-            </Typography>
-            <Typography variant="body1" color="action.active">
-              {t('delegationDetailPage.stationDetail.segregationCode')}
-            </Typography>
-            <Typography variant="body1" fontWeight={'fontWeightMedium'}>
-              {drawerValue?.segregation_code ?? '-'}
-            </Typography>
-            <Typography variant="body1" color="action.active">
-              {t('delegationDetailPage.stationDetail.applicationCode')}
-            </Typography>
-            <Typography variant="body1" fontWeight={'fontWeightMedium'}>
-              {drawerValue?.application_code ?? '-'}
-            </Typography>
-  
-            <Typography variant="overline">
-              {t('delegationDetailPage.stationDetail.references')}
-            </Typography>
-            <Typography variant="body1" color="action.active">
-              {t('delegationDetailPage.stationDetail.status.label')}
-            </Typography>
-            <Grid item xs={8}>
-              {drawerValue?.station_enabled ? (
-                <Chip
-                  label={t('delegationDetailPage.stationDetail.status.active')}
-                  color="success"
-                ></Chip>
-              ) : (
-                <Chip label={t('delegationDetailPage.stationDetail.status.inactive')}></Chip>
-              )}
-            </Grid>
-            <Typography variant="body1" color="action.active">
-              {t('delegationDetailPage.stationDetail.lastUpdate')}
-            </Typography>
-            <Typography variant="body1" fontWeight={'fontWeightMedium'}>
-              {drawerValue?.last_modified_date ?
-              formatDateToDDMMYYYY(drawerValue?.last_modified_date) : '-'}
-            </Typography>
-          </Box>
-        </PaddedDrawer>
-    );
-  
+            {t('delegationDetailPage.stationDetail.goToStationDetails')}
+            <ArrowForwardIcon sx={{ ml: 1 }} fontSize="small" />
+          </ButtonNaked>
+        </Box>
+
+        <Box mt={5}>
+          <Typography variant="overline">
+            {t('delegationDetailPage.stationDetail.references')}
+          </Typography>
+        </Box>
+
+        <Box mt={1}>
+          <Typography variant="body1" color="action.active">
+            {t('delegationDetailPage.stationDetail.status.label')}
+          </Typography>
+          <Grid item xs={8}>
+            {drawerValue?.station_enabled ? (
+              <Chip
+                label={t('delegationDetailPage.stationDetail.status.active')}
+                color="success"
+              ></Chip>
+            ) : (
+              <Chip label={t('delegationDetailPage.stationDetail.status.inactive')}></Chip>
+            )}
+          </Grid>
+        </Box>
+
+        <Box mt={1}>
+          <Typography variant="body1" color="action.active">
+            {t('delegationDetailPage.stationDetail.lastUpdate')}
+          </Typography>
+          <Typography variant="body1" fontWeight={'fontWeightMedium'}>
+            {drawerValue?.last_modified_date
+              ? formatDateToDDMMYYYY(drawerValue?.last_modified_date)
+              : '-'}
+          </Typography>
+        </Box>
+
+        <Box
+          display="flex"
+          flexDirection={'column-reverse'}
+          justifyContent={'space-between'}
+          minHeight="40vh"
+        >
+          <Button
+            fullWidth
+            onClick={() => {
+              setShowDisassociateStationModal(drawerValue?.station_code ?? '');
+              setDrawerValue({});
+            }}
+            color="error"
+            variant="outlined"
+            data-testid="station-detail-disassociate-station-button"
+          >
+            {t('delegationDetailPage.stationDetail.disassociateStaion')}
+          </Button>
+        </Box>
+      </Box>
+    </PaddedDrawer>
+  );
+};

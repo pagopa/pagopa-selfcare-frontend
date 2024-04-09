666535cd18736da70991233c85126b8a23f08c0e
giomella <gioele.mella@emeal.nttdata.com>
2024-04-09 16:34:38 +0200

    [VAS-687] updated error handling and added back button in delegation detail page

--- src/pages/delegations/detail/DelegationDetailPage.tsx
+++ src/pages/delegations/detail/DelegationDetailPage.tsx
@@ -1,4 +1,6 @@
-import { Breadcrumbs, Grid, Typography } from '@mui/material';
+import { ArrowBack } from '@mui/icons-material';
+import { Breadcrumbs, Grid, Stack, Typography } from '@mui/material';
+import { ButtonNaked } from '@pagopa/mui-italia';
 import { TitleBox, useErrorDispatcher, useLoading } from '@pagopa/selfcare-common-frontend';
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -11,6 +13,7 @@ import { Party } from '../../../model/Party';
 import { useAppSelector } from '../../../redux/hooks';
 import { delegationDetailSelectors } from '../../../redux/slices/delegationDetailSlice';
 import { partiesSelectors } from '../../../redux/slices/partiesSlice';
+import ROUTES from '../../../routes';
 import { getCreditorInstitutionContacts } from '../../../services/creditorInstitutionService';
 import { LOADING_TASK_CI_DELEGATION_CONTACTS_LIST } from '../../../utils/constants';
 import DelegationDetailOperativeTable from './DelegationDetailOperativeTable';
@@ -21,6 +24,7 @@ const DelegationDetailPage = () => {
   const { t } = useTranslation();
   const selectedParty: Party | undefined = useAppSelector(partiesSelectors.selectPartySelected);
   const addError = useErrorDispatcher();
+  const history = useHistory();
 
   const [contacts, setContacts] = useState<CreditorInstitutionContactsResource>();
   const [searchInput, setSearchInput] = useState<string>('');
@@ -31,52 +35,66 @@ const DelegationDetailPage = () => {
 
   useEffect(() => {
     setLoading(true);
-    async function getContacts(): Promise<CreditorInstitutionContactsResource> {
-      const ciTaxCode = delegationDetail.institution_tax_code ?? '';
-      const institutionId = delegationDetail.institution_id ?? '';
-      return await getCreditorInstitutionContacts(ciTaxCode, institutionId)
-        .then((contacts) => contacts)
-        .catch((error) => {
-          addError(error);
-          return [];
-        });
-    }
-
-    Promise.all([getContacts()])
-      .then(([contacts]) => {
-        setContacts(contacts);
-      })
-      .catch(() => {})
+    const ciTaxCode = delegationDetail.institution_tax_code ?? '';
+    const institutionId = delegationDetail.institution_id ?? '';
+    getCreditorInstitutionContacts(ciTaxCode, institutionId)
+      .then((contacts) => setContacts(contacts))
+      .catch((reason) =>
+        addError({
+          id: 'DELEGATION_GET_CI_CONTACTS',
+          blocking: false,
+          error: reason as Error,
+          techDescription: `An error occurred while retrieving creditor institution's contacts`,
+          toNotify: true,
+          displayableTitle: t('general.errorTitle'),
+          displayableDescription: t('delegationDetailPage.retrieveContacsErrorMessage'),
+          component: 'Toast',
+        })
+      )
       .finally(() => setLoading(false));
   }, [selectedParty]);
 
   return (
-      <SideMenuLayout>
+    <SideMenuLayout>
+      <Stack direction="row" mt={2}>
+        <ButtonNaked
+          size="small"
+          component="button"
+          onClick={() => history.push(ROUTES.DELEGATIONS_LIST)}
+          startIcon={<ArrowBack />}
+          sx={{ color: 'primary.main', mr: '20px' }}
+          weight="default"
+          data-testid="exit-btn-test"
+        >
+          {t('general.back')}
+        </ButtonNaked>
         <Breadcrumbs>
           <Typography>{t('delegationsPage.title')}</Typography>
           <Typography color={'action.active'}>{delegationDetail.institution_name ?? ''}</Typography>
         </Breadcrumbs>
-        <Grid container mt={1} spacing={1}>
-          <Grid item xs={12}>
-            <TitleBox title={delegationDetail.institution_name ?? ''} variantTitle="h3" />
-          </Grid>
-
-          <Grid item xs={6} mb={1} data-testid="payment-contacts">
-            <DelegationDetailPaymentContacts paymentContacts={contacts?.ci_payment_contacts} />
-          </Grid>
-          <Grid item xs={6} data-testid="operative-table">
-            <DelegationDetailOperativeTable operativeTable={contacts?.operative_table} />
-          </Grid>
+      </Stack>
+      <Grid container mt={1} spacing={1}>
+        <Grid item xs={12}>
+          <TitleBox title={delegationDetail.institution_name ?? ''} variantTitle="h3" />
         </Grid>
 
-        <Grid container mt={5} spacing={1}>
-          <TitleBox title={t('delegationDetailPage.tableTitle')} variantTitle="h4" />
-          <TableSearchBar setSearchInput={setSearchInput} componentName='delegationDetailPage'/>
-          <DelegationStationsTable
-            ciTaxCode={delegationDetail.institution_tax_code ?? ''}
-            filterByStationCode={searchInput}/>
+        <Grid item xs={6} mb={1} data-testid="payment-contacts">
+          <DelegationDetailPaymentContacts paymentContacts={contacts?.ci_payment_contacts} />
         </Grid>
-      </SideMenuLayout>
+        <Grid item xs={6} data-testid="operative-table">
+          <DelegationDetailOperativeTable operativeTable={contacts?.operative_table} />
+        </Grid>
+      </Grid>
+
+      <Grid container mt={5} spacing={1}>
+        <TitleBox title={t('delegationDetailPage.tableTitle')} variantTitle="h4" />
+        <TableSearchBar setSearchInput={setSearchInput} componentName="delegationDetailPage" />
+        <DelegationStationsTable
+          ciTaxCode={delegationDetail.institution_tax_code ?? ''}
+          filterByStationCode={searchInput}
+        />
+      </Grid>
+    </SideMenuLayout>
   );
 };
 

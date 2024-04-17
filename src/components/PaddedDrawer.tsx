60005453d8da236e2c07de5d38cf98df691ff369
svariant <samuele.varianti@nttdata.com>
2024-04-17 18:04:33 +0200

    [VAS-738] Centralized drawer buttons functionality

--- src/components/PaddedDrawer.tsx
+++ src/components/PaddedDrawer.tsx
@@ -1,39 +1,51 @@
-import { Box, Drawer, Stack, IconButton } from "@mui/material";
-import react, { Dispatch, SetStateAction } from "react";
+import { Box, Drawer, Stack, IconButton, Grid } from '@mui/material';
+import react, { Dispatch, SetStateAction } from 'react';
 import CloseIcon from '@mui/icons-material/Close';
 import ArrowBack from '@mui/icons-material/ArrowBack';
 
 export const PaddedDrawer = ({
-    openDrawer,
-    setOpenDrawer,
-    children,
-    hasBackButton,
-    backButtonAction,
-    onClose = () => setOpenDrawer(false)
-  }: {
-    openDrawer: boolean;
-    setOpenDrawer: Dispatch<SetStateAction<boolean>>;
-    children: React.ReactNode;
-    hasBackButton?: boolean;
-    backButtonAction?: React.Dispatch<React.MouseEvent<HTMLButtonElement, MouseEvent>>;
-    onClose?: () => void;
-  }) => (
-    <Drawer open={openDrawer} onClose={() => onClose() } anchor="right" data-testid="padded-drawer">
-      <Box p={3} py={1} sx={{ maxWidth: '420px', minWidth: "420px" }}>
-        <Stack direction="row" justifyContent="space-between">
-            <Box display="flex" justifyContent="flex-start" mb={1}>
-              {hasBackButton &&
-              (<IconButton onClick={backButtonAction} data-testid="back-drawer-button">
-                <ArrowBack />
-              </IconButton>)}
-            </Box>
-            <Box display="flex" justifyContent="flex-end" mb={1}>
-              <IconButton onClick={() => onClose()} data-testid="close-drawer-button">
-                <CloseIcon />
-              </IconButton>
-            </Box>
-        </Stack>
-        {children}
-      </Box>
-    </Drawer>
-  );
+  openDrawer,
+  setOpenDrawer,
+  children,
+  hasBackButton,
+  backButtonAction,
+  onClose = () => setOpenDrawer(false),
+  drawerButtons,
+}: {
+  openDrawer: boolean;
+  setOpenDrawer: Dispatch<SetStateAction<boolean>>;
+  children: React.ReactNode;
+  hasBackButton?: boolean;
+  backButtonAction?: React.Dispatch<React.MouseEvent<HTMLButtonElement, MouseEvent>>;
+  onClose?: () => void;
+  drawerButtons?: React.ReactNode;
+}) => (
+  <Drawer open={openDrawer} onClose={() => onClose()} anchor="right" data-testid="padded-drawer">
+    <Box p={3} py={1} sx={{ maxWidth: '420px', minWidth: '420px', minHeight: '65vh', height: "100%" }}>
+      <Stack direction="row" justifyContent="space-between">
+        <Box display="flex" justifyContent="flex-start" mb={1}>
+          {hasBackButton && (
+            <IconButton onClick={backButtonAction} data-testid="back-drawer-button">
+              <ArrowBack />
+            </IconButton>
+          )}
+        </Box>
+        <Box display="flex" justifyContent="flex-end" mb={1}>
+          <IconButton onClick={() => onClose()} data-testid="close-drawer-button">
+            <CloseIcon />
+          </IconButton>
+        </Box>
+      </Stack>
+      <Grid container height="92%" alignContent={"space-between"}>
+        <Grid item xs={12}>
+          {children}
+        </Grid>
+        {drawerButtons && (
+          <Grid item xs={12} pb={1} height="fit-content">
+            {drawerButtons}
+          </Grid>
+        )}
+      </Grid>
+    </Box>
+  </Drawer>
+);

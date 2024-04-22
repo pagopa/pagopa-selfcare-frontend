36eb52a2747adf115693530d2b0f63ba731bd4aa
Jacopo <jacopo.carlini@gmail.com>
2024-04-22 16:58:57 +0200

    [VAS-913] junit

--- src/components/CommonHeader/__tests__/CommonHeader.test.tsx
+++ src/components/CommonHeader/__tests__/CommonHeader.test.tsx
@@ -1,11 +1,15 @@
-import { JwtUser } from '@pagopa/mui-italia';
-import { render } from '@testing-library/react';
+import {JwtUser} from '@pagopa/mui-italia';
+import {render} from '@testing-library/react';
 import CommonHeader from '../CommonHeader';
+import {isOperator} from '../../../pages/components/commonFunctions';
 
 beforeEach(() => {
   jest.spyOn(console, 'error').mockImplementation(() => {});
   jest.spyOn(console, 'warn').mockImplementation(() => {});
 });
+
+jest.mock('../../../pages/components/commonFunctions.ts');
+
 
 describe('<CommonHeader />', () => {
   const mockedUser: JwtUser = {
@@ -16,10 +20,12 @@ describe('<CommonHeader />', () => {
   };
 
   test('render CommonHeader component', () => {
+    (isOperator as jest.Mock).mockReturnValue(false);
     render(<CommonHeader withSecondHeader={true} loggedUser={mockedUser} />);
   });
 
   test('render CommonHeader component withSecondHeader false', () => {
+    (isOperator as jest.Mock).mockReturnValue(false);
     render(<CommonHeader withSecondHeader={false} loggedUser={mockedUser} />);
   });
 });

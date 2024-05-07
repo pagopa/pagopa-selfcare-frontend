1fa106408dd11ace5c3b5b976103ac93e8076365
svariant <samuele.varianti@nttdata.com>
2024-05-07 17:27:15 +0200

    fix: Button to create bundle in table empty state was rendering for Creditor Institutions

--- src/components/Table/TableEmptyState.tsx
+++ src/components/Table/TableEmptyState.tsx
@@ -1,23 +1,31 @@
 import { Typography } from '@mui/material';
 import { Box } from '@mui/system';
+import React, { ReactNode } from 'react';
 import { useTranslation } from 'react-i18next';
 
 export default function TableEmptyState({
   componentName,
   translationPathSuffix,
-}: Readonly<{ componentName: string; translationPathSuffix?: string }>) {
+  translationArgs,
+  children,
+}: Readonly<{
+  componentName: string;
+  children?: ReactNode;
+  translationPathSuffix?: string;
+  translationArgs?: any;
+}>) {
   const { t } = useTranslation();
   return (
     <Box p={3} mt={3} sx={{ backgroundColor: '#EEEEEE' }} data-testid="empty-state-table">
       <Box p={2} sx={{ textAlign: 'center', backgroundColor: '#FFFFFF' }}>
         <Typography variant="body2">
-          {componentName
-            ? t(
-                `${componentName}.table.emptyState${
-                  translationPathSuffix ? `.${translationPathSuffix}` : ''
-                }`
-              )
-            : ''}
+          {t(
+            `${componentName}.table.emptyState${
+              translationPathSuffix ? `.${translationPathSuffix}` : ''
+            }`,
+            translationArgs
+          )}
+          {children}
         </Typography>
       </Box>
     </Box>

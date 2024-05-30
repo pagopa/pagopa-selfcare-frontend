a9f95c5a062de4ddc743ca1a81099362bdb0b380
svariant <samuele.varianti@nttdata.com>
2024-05-30 15:44:52 +0200

    chore: Reach coverage

--- src/pages/stations/list/__tests__/StationsPage.test.tsx
+++ src/pages/stations/list/__tests__/StationsPage.test.tsx
@@ -1,12 +1,14 @@
 import {ThemeProvider} from '@mui/system';
 import {theme} from '@pagopa/mui-italia';
-import {cleanup, render, screen} from '@testing-library/react';
+import {cleanup, fireEvent, render, screen} from '@testing-library/react';
 import React from 'react';
 import {Router} from 'react-router-dom';
 import {store} from '../../../../redux/store';
 import StationsPage, {clearLocationState} from '../StationsPage';
 import {createMemoryHistory} from 'history';
 import {Provider} from 'react-redux';
+import * as useUserRole from '../../../../hooks/useUserRole';
+import { ROLE } from '../../../../model/RolePermission';
 
 beforeEach(() => {
   jest.spyOn(console, 'error').mockImplementation(() => {});
@@ -18,7 +20,7 @@ afterEach(cleanup);
 describe('<StationsPage />', () => {
   const history = createMemoryHistory();
 
-  test('render component StationsPage', () => {
+  test('render component StationsPage', async () => {
     history.location.state = { alertSuccessMessage: 'Success!' };
     render(
       <Provider store={store}>
@@ -31,6 +33,28 @@ describe('<StationsPage />', () => {
     );
 
     expect(screen.getByTestId('alert-test')).toBeInTheDocument();
+
+    fireEvent.change(screen.getByTestId('search-input'), { target: { value: 'search' } });
+  });
+
+  test('render component StationsPage operator', async () => {
+    jest.spyOn(useUserRole, 'useUserRole').mockReturnValue({
+      userRole: ROLE.PSP_ADMIN,
+      userIsPspAdmin: false,
+      userIsEcAdmin: false,
+      userIsPspDirectAdmin: false,
+      userIsPagopaOperator: true,
+      userIsAdmin: false
+    });
+    render(
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <Router history={history}>
+            <StationsPage />
+          </Router>
+        </ThemeProvider>
+      </Provider>
+    );
   });
 
   it('should replace the current state of window history', () => {

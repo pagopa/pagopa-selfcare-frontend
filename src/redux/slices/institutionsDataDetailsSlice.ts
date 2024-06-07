18c19d64a5eb9eb7ef5c4a47994840ac063e0e7d
Alessio Cialini <alessio.cialini@emeal.nttdata.com>
2024-06-07 14:40:10 +0200

    Fixed focus color on radio for notice form, updated radio button position, updated menu icon

--- src/redux/slices/institutionsDataDetailsSlice.ts
+++ src/redux/slices/institutionsDataDetailsSlice.ts
@@ -2,7 +2,7 @@ import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 import type {RootState} from '../store';
 import {InstitutionUploadData} from '../../api/generated/portal/InstitutionUploadData';
 
-const initialState: InstitutionUploadData = {
+export const initialState: InstitutionUploadData = {
     taxCode: '',
     fullName: '',
     cbill: '',

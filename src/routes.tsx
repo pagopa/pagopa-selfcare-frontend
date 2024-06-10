4e660653ae371191c494ac9c63f83aacbafb3eda
Samuele Varianti <128470180+svariant@users.noreply.github.com>
2024-06-10 15:24:39 +0200

    [VAS-807] feat: Implement Private Bundle add recipients page for PSP (#535)
    * [VAS-776] chore: New command to generate client from local openapi
    
    * [VAS-776] feat: Skeleton implementation Private bundle PSP offers table
    
    * [VAS-776] chore: Cleaned subscription request table using generic datagrid component
    
    * [VAS-776] feat: Generalize bundle request's drawer component & table columns
    
    * [VAS-776] feat: Init unit test CommissionBundleOffersTable
    
    * [VAS-807] feat: Init psp add recipients to private bundle page
    
    * [VAS-807] feat: Implement file dropzone receipients psp private bundles
    
    * [VAS-807] chore: Comment csv dropzone add recipient page private bundles
    
    * [VAS-776] chore: Refactor nomenclature files & variables
    
    * [VAS-776] chore: Update openapi pr
    
    * [VAS-776] chore: Align nomenclature with new backend
    
    * [VAS-776] feat: Unified subscription offers & requests components
    
    * [VAS-776] feat: Implement api delete private bundle offers
    
    * [VAS-807] fix: Merge
    
    * [VAS-807] feat: Align getCreditorInstitutions api
    
    * [VAS-807] feat: Improve ECSelection component for server side autocomplete
    
    * [VAS-807] feat: get creditor institutions sorting
    
    * [VAS-807] feat: Implement error message ECselection
    
    * [VAS-807] feat: Implement createCiBundleOffer api
    
    * [VAS-807] chore: Align with changes in BE class
    
    * [VAS-807] feat: Conclude implementation of CommissionBundleoffersAddRecipientsPage
    
    * [VAS-807] chore: Unit tests
    
    * [VAS-807] fix: Unit test post align with be
    
    * [VAS-807] chore: Unit test StationAssociateECPage
    
    * [VAS-807] feat: Add getCreditorInstitutions new params
    
    * [VAS-807] feat: Improve app selector with redirect to use conditions to validate retrieved redux state

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,54 +1,55 @@
 // import { TOS } from './pages/tos/TOS';
-import {ENV} from './utils/env';
+import { ENV } from './utils/env';
 
 export const BASE_ROUTE = ENV.PUBLIC_URL;
 
 const ROUTES = {
-    AUTH: `${BASE_ROUTE}/auth`,
-    HOME: `${BASE_ROUTE}`,
-    NODE_SIGNIN: `${BASE_ROUTE}/node-signin`,
-    TOS: `${BASE_ROUTE}/terms-of-service`,
-    APIKEYS: `${BASE_ROUTE}/api-keys`,
-    APIKEYS_CREATE: `${BASE_ROUTE}/add-apikey`,
+  AUTH: `${BASE_ROUTE}/auth`,
+  HOME: `${BASE_ROUTE}`,
+  NODE_SIGNIN: `${BASE_ROUTE}/node-signin`,
+  TOS: `${BASE_ROUTE}/terms-of-service`,
+  APIKEYS: `${BASE_ROUTE}/api-keys`,
+  APIKEYS_CREATE: `${BASE_ROUTE}/add-apikey`,
 
-    CHANNELS: `${BASE_ROUTE}/channels`,
-    CHANNEL_DETAIL: `${BASE_ROUTE}/channels/:channelId`,
-    CHANNEL_EDIT: `${BASE_ROUTE}/channels/:channelId/:actionId`,
-    CHANNEL_PSP_LIST: `${BASE_ROUTE}/channels/:channelId/psp-list`,
-    CHANNEL_ASSOCIATE_PSP: `${BASE_ROUTE}/channels/:channelId/associate-psp`,
-    CHANNEL_ADD: `${BASE_ROUTE}/channels/add-channel/`,
+  CHANNELS: `${BASE_ROUTE}/channels`,
+  CHANNEL_DETAIL: `${BASE_ROUTE}/channels/:channelId`,
+  CHANNEL_EDIT: `${BASE_ROUTE}/channels/:channelId/:actionId`,
+  CHANNEL_PSP_LIST: `${BASE_ROUTE}/channels/:channelId/psp-list`,
+  CHANNEL_ASSOCIATE_PSP: `${BASE_ROUTE}/channels/:channelId/associate-psp`,
+  CHANNEL_ADD: `${BASE_ROUTE}/channels/add-channel/`,
 
-    STATIONS: `${BASE_ROUTE}/stations`,
-    STATION_DETAIL: `${BASE_ROUTE}/stations/:stationId`,
-    STATION_EDIT: `${BASE_ROUTE}/stations/:stationId/:actionId`,
-    STATION_EC_LIST: `${BASE_ROUTE}/stations/:stationId/ec-list`,
-    STATION_ASSOCIATE_EC: `${BASE_ROUTE}/stations/:stationId/associate-ec`,
-    STATION_ADD: `${BASE_ROUTE}/stations/add-station/`,
+  STATIONS: `${BASE_ROUTE}/stations`,
+  STATION_DETAIL: `${BASE_ROUTE}/stations/:stationId`,
+  STATION_EDIT: `${BASE_ROUTE}/stations/:stationId/:actionId`,
+  STATION_EC_LIST: `${BASE_ROUTE}/stations/:stationId/ec-list`,
+  STATION_ASSOCIATE_EC: `${BASE_ROUTE}/stations/:stationId/associate-ec`,
+  STATION_ADD: `${BASE_ROUTE}/stations/add-station/`,
 
-    IBAN: `${BASE_ROUTE}/iban`,
-    IBAN_DETAIL: `${BASE_ROUTE}/iban/:ibanId`,
-    IBAN_EDIT: `${BASE_ROUTE}/iban/:ibanId/:actionId`,
-    IBAN_ADD: `${BASE_ROUTE}/iban/add-iban/`,
+  IBAN: `${BASE_ROUTE}/iban`,
+  IBAN_DETAIL: `${BASE_ROUTE}/iban/:ibanId`,
+  IBAN_EDIT: `${BASE_ROUTE}/iban/:ibanId/:actionId`,
+  IBAN_ADD: `${BASE_ROUTE}/iban/add-iban/`,
 
-    COMMISSION_BUNDLES: `${BASE_ROUTE}/comm-bundles`,
-    COMMISSION_BUNDLES_DETAIL: `${BASE_ROUTE}/comm-bundles/detail/:bundleId`,
-    COMMISSION_BUNDLES_ADD: `${BASE_ROUTE}/comm-bundles/add-bundle/`,
-    COMMISSION_BUNDLES_EDIT: `${BASE_ROUTE}/comm-bundles/:bundleId/:actionId`,
-    COMMISSION_BUNDLES_ACTIVATE: `${BASE_ROUTE}/comm-bundles/activate-bundle/`,
+  COMMISSION_BUNDLES: `${BASE_ROUTE}/comm-bundles`,
+  COMMISSION_BUNDLES_DETAIL: `${BASE_ROUTE}/comm-bundles/detail/:bundleId`,
+  COMMISSION_BUNDLES_ADD_RECIPIENT: `${BASE_ROUTE}/comm-bundles/detail/:bundleId/add-recipient`,
+  COMMISSION_BUNDLES_ADD: `${BASE_ROUTE}/comm-bundles/add-bundle/`,
+  COMMISSION_BUNDLES_EDIT: `${BASE_ROUTE}/comm-bundles/:bundleId/:actionId`,
+  COMMISSION_BUNDLES_ACTIVATE: `${BASE_ROUTE}/comm-bundles/activate-bundle/`,
 
-    OPERATION_TABLE_ADDEDIT: `${BASE_ROUTE}/operation-table/addedit`,
-    OPERATION_TABLE_DETAILS: `${BASE_ROUTE}/operation-table/:operationTableId`,
-    OPERATION_TABLE_LIST: `${BASE_ROUTE}/operation-table-list`,
+  OPERATION_TABLE_ADDEDIT: `${BASE_ROUTE}/operation-table/addedit`,
+  OPERATION_TABLE_DETAILS: `${BASE_ROUTE}/operation-table/:operationTableId`,
+  OPERATION_TABLE_LIST: `${BASE_ROUTE}/operation-table-list`,
 
-    DELEGATIONS_LIST: `${BASE_ROUTE}/delegation-list`,
-    DELEGATIONS_DETAIL: `${BASE_ROUTE}/delegation-list/detail`,
+  DELEGATIONS_LIST: `${BASE_ROUTE}/delegation-list`,
+  DELEGATIONS_DETAIL: `${BASE_ROUTE}/delegation-list/detail`,
 
-    PAYMENTS_RECEIPTS: `${BASE_ROUTE}/payments-receipts`,
+  PAYMENTS_RECEIPTS: `${BASE_ROUTE}/payments-receipts`,
 
-    PAYMENT_NOTICES: `${BASE_ROUTE}/payments-notices`,
-    PAYMENT_NOTICES_ADDEDIT: `${BASE_ROUTE}/payments-notices/addedit`,
+  PAYMENT_NOTICES: `${BASE_ROUTE}/payments-notices`,
+  PAYMENT_NOTICES_ADDEDIT: `${BASE_ROUTE}/payments-notices/addedit`,
 
-    /* TOS: {
+  /* TOS: {
       PATH: '/terms-of-service',
       LABEL: { it: 'Termini di servizio', en: 'Terms of service' },
       COMPONENT: TOS,

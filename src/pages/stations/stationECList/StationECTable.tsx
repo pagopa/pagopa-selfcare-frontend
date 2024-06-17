b23cb3c33706958e96929c6e208ef20d471bf143
svariant <samuele.varianti@nttdata.com>
2024-06-17 15:57:00 +0200

    [VAS-1112] chore: Clean StationECTable code using generic components

--- src/pages/stations/stationECList/StationECTable.tsx
+++ src/pages/stations/stationECList/StationECTable.tsx
@@ -1,225 +1,159 @@
-import {Box, Pagination, Typography} from '@mui/material';
-import {GridColDef} from '@mui/x-data-grid';
-import {SessionModal, useErrorDispatcher, useLoading} from '@pagopa/selfcare-common-frontend';
-import {handleErrors} from '@pagopa/selfcare-common-frontend/services/errorService';
-import {ChangeEvent, useEffect, useState} from 'react';
-import {Trans, useTranslation} from 'react-i18next';
-import {useParams} from 'react-router';
-import {CreditorInstitutionsResource} from '../../../api/generated/portal/CreditorInstitutionsResource';
-import {CustomDataGrid} from '../../../components/Table/TableDataGrid';
-import {dissociateECfromStation, getECListByStationCode} from '../../../services/stationService';
-import {LOADING_TASK_STATION_EC_TABLE} from '../../../utils/constants';
-import {buildColumnDefs} from './StationECTableColumns';
-import StationECTableEmpty from './StationECTableEmpty';
+import { Box } from '@mui/material';
+import { GridColDef } from '@mui/x-data-grid';
+import { SessionModal, useErrorDispatcher, useLoading } from '@pagopa/selfcare-common-frontend';
+import { handleErrors } from '@pagopa/selfcare-common-frontend/services/errorService';
+import { useEffect, useState } from 'react';
+import { Trans, useTranslation } from 'react-i18next';
+import { useParams } from 'react-router';
+import { generatePath } from 'react-router-dom';
+import ROUTES from '../../../routes';
+import { CreditorInstitutionsResource } from '../../../api/generated/portal/CreditorInstitutionsResource';
+import TableDataGrid from '../../../components/Table/TableDataGrid';
+import { dissociateECfromStation, getECListByStationCode } from '../../../services/stationService';
+import { LOADING_TASK_STATION_EC_TABLE } from '../../../utils/constants';
+import { buildColumnDefs } from './StationECTableColumns';
 
 const rowHeight = 64;
 const headerHeight = 56;
 
 const emptyECList: CreditorInstitutionsResource = {
-    creditor_institutions: [],
-    page_info: {
-        page: 0,
-        limit: 50,
-        items_found: 0,
-        total_pages: 0,
-    },
+  creditor_institutions: [],
+  page_info: {
+    page: 0,
+    limit: 50,
+    items_found: 0,
+    total_pages: 0,
+  },
 };
 
 type StationECTableProps = {
-    setAlertMessage: any;
-    ciNameOrFiscalCodeFilter: string;
+  setAlertMessage: any;
+  ciNameOrFiscalCodeFilter: string;
 };
 
+const componentPath = 'stationECList';
 export default function StationECTable({
-                                           setAlertMessage,
-                                           ciNameOrFiscalCodeFilter,
-                                       }: StationECTableProps) {
-    const {t} = useTranslation();
-    const [showConfirmModal, setShowConfirmModal] = useState({show: false, data: ''});
-    const [error, setError] = useState(false);
+  setAlertMessage,
+  ciNameOrFiscalCodeFilter,
+}: StationECTableProps) {
+  const { t } = useTranslation();
+  const [showConfirmModal, setShowConfirmModal] = useState({ show: false, data: '' });
+  const [error, setError] = useState(false);
 
-    const [loading, setLoading] = useState(false);
-    const setLoadingOverlay = useLoading(LOADING_TASK_STATION_EC_TABLE);
-    const setLoadingStatus = (status: boolean) => {
-        setLoading(status);
-        setLoadingOverlay(status);
-    };
+  const setLoadingOverlay = useLoading(LOADING_TASK_STATION_EC_TABLE);
 
-    const addError = useErrorDispatcher();
+  const addError = useErrorDispatcher();
 
-    const [ecListPage, setECListPage] = useState<CreditorInstitutionsResource>(emptyECList);
-    const [page, setPage] = useState<number>(0);
-    const [selectedECCode, setSelectedECCode] = useState<string>('');
+  const [ecListPage, setECListPage] = useState<CreditorInstitutionsResource>(emptyECList);
+  const [page, setPage] = useState<number>(0);
+  const [selectedECCode, setSelectedECCode] = useState<string>('');
+  const [pageLimit, setPageLimit] = useState<number>(5);
 
-    const {stationId} = useParams<{ stationId: string }>();
+  const { stationId } = useParams<{ stationId: string }>();
 
-    useEffect(() => {
-        fetchStationECs(0);
-    }, [ciNameOrFiscalCodeFilter]);
+  const onRowClick = (ecIdRow: string) => {
+    setSelectedECCode(ecIdRow);
+    setShowConfirmModal({
+      show: true,
+      data:
+        ecListPage?.creditor_institutions?.find((item) => item.ciTaxCode === ecIdRow)
+          ?.businessName ?? '',
+    });
+  };
+  const columns: Array<GridColDef> = buildColumnDefs(t, onRowClick);
 
-    const onRowClick = (ecIdRow: string) => {
-        setSelectedECCode(ecIdRow);
-        setShowConfirmModal({
-            show: true,
-            data:
-                ecListPage?.creditor_institutions?.find((item) => item.ciTaxCode === ecIdRow)
-                    ?.businessName ?? '',
-        });
-    };
-    const columns: Array<GridColDef> = buildColumnDefs(t, onRowClick);
+  const dissociateEC = async () => {
+    setShowConfirmModal({ show: false, data: '' });
 
-    const dissociateEC = async () => {
-        setShowConfirmModal({show: false, data: ''});
-        setLoading(true);
+    try {
+      await dissociateECfromStation(selectedECCode, stationId);
+      setAlertMessage(t(`${componentPath}.dissociateEcSuccessMessage`));
+      fetchStationECs(page);
+    } catch (reason) {
+      addError({
+        id: 'STATION_DELETE_RELATIONSHIP',
+        blocking: false,
+        error: reason as Error,
+        techDescription: `An error occurred while deleting relationship between EC and Station`,
+        toNotify: true,
+        displayableTitle: t(`${componentPath}.dissociateEcErrorTitle`),
+        displayableDescription: t(`${componentPath}.dissociateEcErrorMessage`),
+        component: 'Toast',
+      });
+    } finally {
+      setSelectedECCode('');
+    }
+  };
 
-        try {
-            await dissociateECfromStation(selectedECCode, stationId);
-            setAlertMessage(t('stationECList.dissociateEcSuccessMessage'));
-            fetchStationECs(page);
-        } catch (reason) {
-            addError({
-                id: 'STATION_DELETE_RELATIONSHIP',
-                blocking: false,
-                error: reason as Error,
-                techDescription: `An error occurred while deleting relationship between EC and Station`,
-                toNotify: true,
-                displayableTitle: t('stationECList.dissociateEcErrorTitle'),
-                displayableDescription: t('stationECList.dissociateEcErrorMessage'),
-                component: 'Toast',
-            });
-        } finally {
-            setSelectedECCode('');
-            setLoading(false);
+  const fetchStationECs = (currentPage: number) => {
+    setLoadingOverlay(true);
+    setPage(currentPage);
+
+    getECListByStationCode(stationId, ciNameOrFiscalCodeFilter, currentPage)
+      .then((r) => (r ? setECListPage(r) : setECListPage(emptyECList)))
+      .catch((reason) => {
+        handleErrors([
+          {
+            id: `FETCH_STATIONS_ERROR`,
+            blocking: false,
+            error: reason,
+            techDescription: `An error occurred while fetching stations`,
+            toNotify: false,
+          },
+        ]);
+        setError(true);
+        setECListPage(emptyECList);
+      })
+      .finally(() => setLoadingOverlay(false));
+  };
+
+  useEffect(() => {
+    fetchStationECs(0);
+  }, [ciNameOrFiscalCodeFilter, pageLimit]);
+
+  return (
+    <>
+      <Box
+        id="StationsSearchTableBox"
+        sx={{
+          position: 'relative',
+          width: '100% !important',
+          border: 'none',
+        }}
+        justifyContent="start"
+      >
+        <TableDataGrid
+          componentPath={componentPath}
+          linkToRedirect={generatePath(ROUTES.STATION_ASSOCIATE_EC, {
+            stationId,
+          })}
+          rows={[...(ecListPage?.creditor_institutions ?? [])]}
+          columns={columns}
+          totalPages={ecListPage?.page_info?.total_pages}
+          page={page}
+          handleChangePage={(newPage: number) => fetchStationECs(newPage)}
+          pageLimit={pageLimit}
+          setPageLimit={setPageLimit}
+          getRowId={(r) => r.ciTaxCode}
+        />
+      </Box>
+      <SessionModal
+        open={showConfirmModal.show}
+        title={t(`${componentPath}.dissociateModal.title`)}
+        message={
+          <Trans
+            i18nKey="stationECList.dissociateModal.message"
+            values={{ ecName: showConfirmModal.data }}
+            defaults="Se dissoci {{ ecName }} sarà disattivata la sua connessione alla stazione."
+          />
         }
-    };
-
-    const fetchStationECs = (currentPage: number) => {
-        setLoadingStatus(true);
-        setPage(currentPage);
-
-        getECListByStationCode(stationId, ciNameOrFiscalCodeFilter, currentPage)
-            .then((r) => (r ? setECListPage(r) : setECListPage(emptyECList)))
-            .catch((reason) => {
-                handleErrors([
-                    {
-                        id: `FETCH_STATIONS_ERROR`,
-                        blocking: false,
-                        error: reason,
-                        techDescription: `An error occurred while fetching stations`,
-                        toNotify: false,
-                    },
-                ]);
-                setError(true);
-                setECListPage(emptyECList);
-            })
-            .finally(() => setLoadingStatus(false));
-    };
-
-    return (
-        <>
-            <Box
-                id="StationsSearchTableBox"
-                sx={{
-                    position: 'relative',
-                    width: '100% !important',
-                    border: 'none',
-                }}
-                justifyContent="start"
-            >
-                {error && !loading ? (
-                    <>{error}</>
-                ) : !error && !loading && ecListPage.creditor_institutions?.length === 0 ? (
-                    <StationECTableEmpty stationId={stationId}/>
-                ) : (
-                    <div data-testId='table-data-grid'>
-                        <CustomDataGrid
-                            disableColumnFilter
-                            disableColumnSelector
-                            disableDensitySelector
-                            disableSelectionOnClick
-                            autoHeight={true}
-                            className="CustomDataGrid"
-                            columnBuffer={6}
-                            columns={columns}
-                            components={{
-                                Pagination: () =>
-                                    ecListPage.page_info?.total_pages && ecListPage.page_info?.total_pages > 1 ? (
-                                        <Pagination
-                                            color="primary"
-                                            count={ecListPage?.page_info?.total_pages ?? 1}
-                                            page={page + 1}
-                                            onChange={(_event: ChangeEvent<unknown>, value: number) => setPage(value - 1)}
-                                        />
-                                    ) : (
-                                        <></>
-                                    ),
-                                Toolbar: () => <></>,
-                                NoRowsOverlay: () => (
-                                    <>
-                                        <Box p={2} sx={{textAlign: 'center', backgroundColor: '#FFFFFF'}}>
-                                            <Typography variant="body2">
-                                                {loading ? (
-                                                    <Trans i18nKey="stationECList.table.loading">Loading...</Trans>
-                                                ) : (
-                                                    <Trans i18nKey="stationECList.noResults">No results</Trans>
-                                                )}
-                                            </Typography>
-                                        </Box>
-                                    </>
-                                ),
-                                NoResultsOverlay: () => (
-                                    <>
-                                        <Box p={2} sx={{textAlign: 'center', backgroundColor: '#FFFFFF'}}>
-                                            <Typography variant="body2">
-                                                {loading ? (
-                                                    <Trans i18nKey="stationECList.table.loading">Loading...</Trans>
-                                                ) : (
-                                                    <Trans i18nKey="stationECList.noSearchResults">No search
-                                                        results</Trans>
-                                                )}
-                                            </Typography>
-                                        </Box>
-                                    </>
-                                ),
-                            }}
-                            componentsProps={{
-                                toolbar: {
-                                    quickFilterProps: {debounceMs: 500},
-                                },
-                            }}
-                            getRowId={(r) => r.ciTaxCode}
-                            headerHeight={headerHeight}
-                            hideFooterSelectedRowCount={true}
-                            paginationMode="server"
-                            rowsPerPageOptions={[3]}
-                            onPageChange={(newPage) => fetchStationECs(newPage)}
-                            pageSize={3}
-                            pagination
-                            rowHeight={rowHeight}
-                            rows={ecListPage.creditor_institutions ?? []}
-                            rowCount={ecListPage!.page_info!.items_found}
-                            sortingMode="server"
-                        />
-                    </div>
-                )}
-            </Box>
-            <SessionModal
-                open={showConfirmModal.show}
-                title={t('stationECList.dissociateModal.title')}
-                message={
-                    <Trans
-                        i18nKey="stationECList.dissociateModal.message"
-                        values={{ecName: showConfirmModal.data}}
-                        defaults="Se dissoci {{ ecName }} sarà disattivata la sua connessione alla stazione."
-                    />
-                }
-                onConfirmLabel={t('stationECList.dissociateModal.confirmButton')}
-                onCloseLabel={t('stationECList.dissociateModal.cancelButton')}
-                onConfirm={dissociateEC}
-                handleClose={() => {
-                    setShowConfirmModal({show: false, data: ''});
-                }}
-            />
-        </>
-    );
+        onConfirmLabel={t(`${componentPath}.dissociateModal.confirmButton`)}
+        onCloseLabel={t(`${componentPath}.dissociateModal.cancelButton`)}
+        onConfirm={dissociateEC}
+        handleClose={() => {
+          setShowConfirmModal({ show: false, data: '' });
+        }}
+      />
+    </>
+  );
 }

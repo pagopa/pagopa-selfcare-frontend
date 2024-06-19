1b43963b16333c5200a1e0b64cfb0dc1f5fa1cff
Jacopo <jacopo.carlini@gmail.com>
2024-06-19 14:51:31 +0200

    [VAS-1112] feat: Change getAvailableCreditorInstitutionsForStation API implementation (#568)
    * [VAS-1112] feat: Align with BE new property nomenclature
    
    * [VAS-1112] feat: Change API getAvailableCreditorInstitutionsForStation implementation with server side autocomplete
    
    * Bump to version 1.25.2-2-VAS-1112-api-get-delegation-changes [skip ci]
    
    * [VAS-1112] feat: Improve autocomplete input timeout
    
    * [VAS-1112] feat: Implemented call to api to abilitate associateEc button
    
    * [VAS-1112] chore: Clean StationECTable code using generic components
    
    * [VAS-1112] chore: Fix unit tests
    
    * [VAS-1112] feat: Improve ec selection
    
    * [VAS-1112] chore: Delete unused files
    
    * [VAS-1112] added tests
    
    * [VAS-1112] chore: clean code
    
    ---------
    
    Co-authored-by: pagopa-github-bot <github-bot@pagopa.it>
    Co-authored-by: giomella <giomella@emeal.nttdata.com>
    # Conflicts:
    #	package.json

--- src/pages/stations/stationECList/StationECPage.tsx
+++ src/pages/stations/stationECList/StationECPage.tsx
@@ -1,120 +1,175 @@
-import {ArrowBack, FileDownload} from '@mui/icons-material';
-import {Alert, Box, Breadcrumbs, Button, Grid, Stack, Typography} from '@mui/material';
-import {ButtonNaked} from '@pagopa/mui-italia';
-import {TitleBox} from '@pagopa/selfcare-common-frontend';
-import {useEffect, useState} from 'react';
-import {useTranslation} from 'react-i18next';
-import {useHistory, useParams} from 'react-router';
+import { Add, ArrowBack, FileDownload } from '@mui/icons-material';
+import { Alert, Box, Breadcrumbs, Button, Grid, Stack, Typography } from '@mui/material';
+import { ButtonNaked } from '@pagopa/mui-italia';
+import { TitleBox, useErrorDispatcher } from '@pagopa/selfcare-common-frontend';
+import { useEffect, useState } from 'react';
+import { generatePath, Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { useHistory, useParams } from 'react-router';
 import SideMenuLayout from '../../../components/SideMenu/SideMenuLayout';
 import ROUTES from '../../../routes';
+import TableSearchBar from '../../../components/Table/TableSearchBar';
 // import { mockedStationECs } from '../../../services/__mocks__/stationService';
+import { CreditorInstitutionInfoResource } from '../../../api/generated/portal/CreditorInstitutionInfoResource';
+import { useAppSelector } from '../../../redux/hooks';
+import { partiesSelectors } from '../../../redux/slices/partiesSlice';
+import { getAvailableCreditorInstitutionsForStation } from '../../../services/creditorInstitutionService';
 import StationECTable from './StationECTable';
-import StationECTableSearchBar from './StationECTableSearchBar';
-
 
 const StationECListPage = () => {
-    const {t} = useTranslation();
-    const history = useHistory();
-    const {stationId} = useParams<{ stationId: string }>();
-    const [alertMessage, setAlertMessage] = useState('');
-    const goBack = () => history.push(ROUTES.STATIONS);
+  const { t } = useTranslation();
+  const history = useHistory();
+  const { stationId } = useParams<{ stationId: string }>();
+  const [alertMessage, setAlertMessage] = useState('');
+  const goBack = () => history.push(ROUTES.STATIONS);
+  const selectedParty = useAppSelector(partiesSelectors.selectPartySelected);
+  const addError = useErrorDispatcher();
+  const [ciNameOrFiscalCodeInput, setCiNameInput] = useState<string>('');
+  const [ciNameOrFiscalCodeFilter, setCiNameOrFiscalCodeFilter] = useState<string>('');
+  const [noValidCi, setNoValidCi] = useState<boolean | undefined>();
 
-    const [ciNameOrFiscalCodeInput, setCiNameInput] = useState<string>('');
-    const [ciNameOrFiscalCodeFilter, setCiNameOrFiscalCodeFilter] = useState<string>('');
+  useEffect(() => {
+    const setSearchValue = setTimeout(() => {
+      setCiNameOrFiscalCodeFilter(ciNameOrFiscalCodeInput);
+    }, 500);
 
-    useEffect(() => {
-        const setSearchValue = setTimeout(() => {
-            setCiNameOrFiscalCodeFilter(ciNameOrFiscalCodeInput);
-        }, 500);
+    return () => clearTimeout(setSearchValue);
+  }, [ciNameOrFiscalCodeInput]);
 
-        return () => clearTimeout(setSearchValue);
-    }, [ciNameOrFiscalCodeInput]);
+  useEffect(() => {
+    if (history.location.state && (history.location.state as any).alertSuccessMessage) {
+      setAlertMessage((history.location.state as any).alertSuccessMessage);
+      window.history.replaceState({}, document.title);
+    }
+  }, []);
 
-    useEffect(() => {
-        if (history.location.state && (history.location.state as any).alertSuccessMessage) {
-            setAlertMessage((history.location.state as any).alertSuccessMessage);
-            window.history.replaceState({}, document.title);
+  useEffect(() => {
+    if (selectedParty?.partyId) {
+      getAvailableCreditorInstitutionsForStation({
+        stationCode: stationId,
+        brokerId: selectedParty.partyId,
+      })
+        .then((data: CreditorInstitutionInfoResource) => {
+          if (
+            data?.creditor_institution_info_list &&
+            data.creditor_institution_info_list.length > 0
+          ) {
+            setNoValidCi(false);
+          } else {
+            setNoValidCi(true);
+          }
+        })
+        .catch((reason) =>
+          addError({
+            id: 'GET_AVAILABLE_DELEGATED_EC_LIST',
+            blocking: false,
+            error: reason,
+            techDescription: `An error occurred while getting delegated ec list`,
+            toNotify: true,
+            displayableTitle: t('general.errorTitle'),
+            displayableDescription: t(
+              'stationAssociateECPage.associationForm.errorMessageDelegatedEd'
+            ),
+            component: 'Toast',
+          })
+        );
+    }
+  }, []);
+
+  setTimeout(() => setAlertMessage(''), 6000);
+
+  const downloadCSV = () => {
+    // TODO: fetch form station service
+  };
+
+  return (
+    <SideMenuLayout>
+      <Stack direction="row" mb={3}>
+        <ButtonNaked
+          size="small"
+          component="button"
+          onClick={goBack}
+          startIcon={<ArrowBack />}
+          sx={{ color: 'primary.main', mr: '20px', fontWeight: 700 }}
+          weight="default"
+        >
+          {t('general.back')}
+        </ButtonNaked>
+        <Breadcrumbs>
+          <Typography fontSize={16}>{stationId}</Typography>
+          <Typography fontWeight={'fontWeightMedium'}>{t('stationECList.title')}</Typography>
+        </Breadcrumbs>
+      </Stack>
+
+      <Stack direction="row" justifyContent={'space-between'}>
+        <Box>
+          <TitleBox
+            title={t('stationECList.title')}
+            subTitle={t('stationECList.subtitle')}
+            mbTitle={2}
+            mbSubTitle={3}
+            variantTitle="h4"
+            variantSubTitle="body1"
+          />
+        </Box>
+        <Box>
+          <Button
+            variant="outlined"
+            sx={{ display: 'none' }}
+            startIcon={<FileDownload />}
+            onClick={() => downloadCSV()}
+          >
+            {t('stationECList.csvDownload')}
+          </Button>
+        </Box>
+      </Stack>
+
+      {alertMessage && (
+        <Alert
+          sx={{
+            position: 'fixed',
+            bottom: '20px',
+            right: '20px',
+            zIndex: 1000,
+          }}
+          severity="success"
+          variant="outlined"
+        >
+          {alertMessage}
+        </Alert>
+      )}
+      {noValidCi && (
+        <Box mb={2}>
+          <Alert severity={'warning'} data-testid="alert-warning-test" variant="outlined">
+            {t('stationAssociateECPage.alert.noDelegations')}
+          </Alert>
+        </Box>
+      )}
+
+      <TableSearchBar
+        componentName="stationECList"
+        setExternalSearchInput={setCiNameInput}
+        customEndButton={
+          <Button
+            component={Link}
+            to={generatePath(ROUTES.STATION_ASSOCIATE_EC, {
+              stationId,
+            })}
+            variant="contained"
+            sx={{ ml: 1, whiteSpace: 'nowrap', minWidth: 'auto' }}
+            startIcon={<Add />}
+            disabled={noValidCi ?? true}
+          >
+            {t('stationECList.associateEcButtonLabel')}
+          </Button>
         }
-    }, []);
-
-    setTimeout(() => setAlertMessage(''), 6000);
-
-    const downloadCSV = () => {
-        // TODO: fetch form station service
-    };
-
-    return (
-        <SideMenuLayout>
-            <Stack direction="row" mb={3}>
-                <ButtonNaked
-                    size="small"
-                    component="button"
-                    onClick={goBack}
-                    startIcon={<ArrowBack/>}
-                    sx={{color: 'primary.main', mr: '20px', fontWeight: 700}}
-                    weight="default"
-                >
-                    {t('general.back')}
-                </ButtonNaked>
-                <Breadcrumbs>
-                    <Typography fontSize={16}>{stationId}</Typography>
-                    <Typography fontWeight={'fontWeightMedium'}>{t('stationECList.title')}</Typography>
-                </Breadcrumbs>
-            </Stack>
-
-            <Stack direction="row" justifyContent={'space-between'}>
-                <Box>
-                    <TitleBox
-                        title={t('stationECList.title')}
-                        subTitle={t('stationECList.subtitle')}
-                        mbTitle={2}
-                        mbSubTitle={3}
-                        variantTitle="h4"
-                        variantSubTitle="body1"
-                    />
-                </Box>
-                <Box>
-                    <Button
-                        variant="outlined"
-                        sx={{display: 'none'}}
-                        startIcon={<FileDownload/>}
-                        onClick={() => downloadCSV()}
-                    >
-                        {t('stationECList.csvDownload')}
-                    </Button>
-                </Box>
-            </Stack>
-
-            {alertMessage && (
-                <Alert
-                    sx={{
-                        position: 'fixed',
-                        bottom: '20px',
-                        right: '20px',
-                        zIndex: 1000,
-                    }}
-                    severity="success"
-                    variant="outlined"
-                >
-                    {alertMessage}
-                </Alert>
-            )}
-
-            <StationECTableSearchBar
-                stationId={stationId}
-                ciNameOrFiscalCodeInput={ciNameOrFiscalCodeInput}
-                setCiNameInput={setCiNameInput}
-            />
-            <Box display="flex" width="100%" mt={0}>
-                <Box pt={0} display="flex" width="100%">
-                    <StationECTable
-                        ciNameOrFiscalCodeFilter={ciNameOrFiscalCodeFilter}
-                        setAlertMessage={setAlertMessage}
-                    />
-                </Box>
-            </Box>
-        </SideMenuLayout>
-    );
+      />
+      <StationECTable
+        ciNameOrFiscalCodeFilter={ciNameOrFiscalCodeFilter}
+        setAlertMessage={setAlertMessage}
+      />
+    </SideMenuLayout>
+  );
 };
 
 export default StationECListPage;

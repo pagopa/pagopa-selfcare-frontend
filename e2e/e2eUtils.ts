2b767405e31d22fe4c191a423a016ad1aa107bae
svariant <samuele.varianti@nttdata.com>
2024-06-19 17:45:07 +0200

    [VAS-1095] fix: Changed timeouts

--- e2e/e2eUtils.ts
+++ e2e/e2eUtils.ts
@@ -15,7 +15,7 @@ export const PSP_DEMO_DIRECT_CODE = 'ABI50004';
 
 export async function login(page: Page) {
   await page.goto('https://dev.selfcare.pagopa.it/dashboard');
-  await page.waitForTimeout(2000);
+  await page.waitForTimeout(1000);
   if (await page.getByRole('button', { name: 'Chiudi' }).isVisible()) {
     await page.getByRole('button', { name: 'Chiudi' }).click();
   }
@@ -54,6 +54,7 @@ export async function goToStart(page: Page) {
 }
 
 export async function changeToEcUser(page: Page) {
+  await page.waitForTimeout(2000);
   await page.getByRole('button', { name: 'Comune di Frosinone Referente' }).click();
   await page.getByLabel('Cerca ente').click();
   await page.getByLabel('Cerca ente').fill('EC demo');

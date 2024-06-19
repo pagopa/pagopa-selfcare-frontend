296318082b739b7da51994fa6ea1becffdc33533
giomella <giomella@emeal.nttdata.com>
2024-06-19 16:20:42 +0200

    [VAS-997] updated tests

--- src/pages/channels/list/__tests__/ChannelsTable.test.tsx
+++ src/pages/channels/list/__tests__/ChannelsTable.test.tsx
@@ -1,66 +1,65 @@
-import {ThemeProvider} from '@mui/system';
-import {theme} from '@pagopa/mui-italia';
-import {cleanup, render, waitFor, screen} from '@testing-library/react';
+import { ThemeProvider } from '@mui/system';
+import { theme } from '@pagopa/mui-italia';
+import { cleanup, render, screen, waitFor } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
 import React from 'react';
-import {Router} from 'react-router-dom';
-import {store} from '../../../../redux/store';
-import ChannelsTable from '../ChannelsTable';
-import {createMemoryHistory} from 'history';
-import {Provider} from 'react-redux';
-import {pspAdminSignedDirect} from '../../../../services/__mocks__/partyService';
-import {partiesActions} from '../../../../redux/slices/partiesSlice';
-import {useAppDispatch} from '../../../../redux/hooks';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import { ConfigurationStatus } from '../../../../model/Station';
+import { useAppDispatch } from '../../../../redux/hooks';
+import { partiesActions } from '../../../../redux/slices/partiesSlice';
+import { store } from '../../../../redux/store';
+import { mockedChannels } from '../../../../services/__mocks__/channelService';
+import { pspAdminSignedDirect } from '../../../../services/__mocks__/partyService';
 import * as ChannelService from '../../../../services/channelService';
-import {mockedChannelsMerged} from '../../../../services/__mocks__/channelService';
+import ChannelsTable from '../ChannelsTable';
 
 const mockGetChannelsMerged = jest.spyOn(ChannelService, 'getChannelsMerged');
 
 beforeEach(() => {
-    jest.spyOn(console, 'error').mockImplementation(() => {
-    });
-    jest.spyOn(console, 'warn').mockImplementation(() => {
-    });
+  jest.spyOn(console, 'error').mockImplementation(() => {});
+  jest.spyOn(console, 'warn').mockImplementation(() => {});
 });
 
 afterEach(cleanup);
 
 const Component = () => {
-    const history = createMemoryHistory();
-    const dispatch = useAppDispatch();
-    dispatch(partiesActions.setPartySelected(pspAdminSignedDirect));
+  const history = createMemoryHistory();
+  const dispatch = useAppDispatch();
+  dispatch(partiesActions.setPartySelected(pspAdminSignedDirect));
 
-    return (
-        <ThemeProvider theme={theme}>
-            <Router history={history}>
-                <ChannelsTable channelCodeFilter={'1'}/>
-            </Router>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <Router history={history}>
+        <ChannelsTable channelCodeFilter={'1'} statusFilter={ConfigurationStatus.ACTIVE} />
+      </Router>
+    </ThemeProvider>
+  );
 };
 
 describe('<ChannelsTable />', () => {
-    test('render component ChannelsTable', async () => {
-        mockGetChannelsMerged.mockReturnValue(Promise.resolve(mockedChannelsMerged));
-        render(
-            <Provider store={store}>
-                <Component/>
-            </Provider>
-        );
+  test('render component ChannelsTable', async () => {
+    mockGetChannelsMerged.mockReturnValue(Promise.resolve(mockedChannels));
+    render(
+      <Provider store={store}>
+        <Component />
+      </Provider>
+    );
 
-        await waitFor(() => {
-            expect(screen.queryByTestId('empty-state-table')).not.toBeInTheDocument();
-        });
+    await waitFor(() => {
+      expect(screen.queryByTestId('empty-state-table')).not.toBeInTheDocument();
     });
-    test('render component ChannelsTable error getChannelsMerged', async () => {
-        mockGetChannelsMerged.mockRejectedValue('error');
-        render(
-            <Provider store={store}>
-                <Component/>
-            </Provider>
-        );
+  });
+  test('render component ChannelsTable error getChannelsMerged', async () => {
+    mockGetChannelsMerged.mockRejectedValue('error');
+    render(
+      <Provider store={store}>
+        <Component />
+      </Provider>
+    );
 
-        await waitFor(() => {
-            expect(screen.queryByTestId('empty-state-table')).toBeInTheDocument();
-        });
+    await waitFor(() => {
+      expect(screen.queryByTestId('empty-state-table')).toBeInTheDocument();
     });
+  });
 });

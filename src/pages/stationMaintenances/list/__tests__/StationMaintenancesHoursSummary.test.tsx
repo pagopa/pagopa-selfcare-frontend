06340d997e95b30f586f74503c64d115190b0d3b
svariant <samuele.varianti@nttdata.com>
2024-08-02 14:21:45 +0200

    [PPANTT-42] fix: Unit tests

--- src/pages/stationMaintenances/list/__tests__/StationMaintenancesHoursSummary.test.tsx
+++ src/pages/stationMaintenances/list/__tests__/StationMaintenancesHoursSummary.test.tsx
@@ -2,28 +2,21 @@ import { render } from '@testing-library/react';
 import React from 'react';
 import { Provider } from 'react-redux';
 import StationMaintenancesHoursSummary from '../StationMaintenancesHoursSummary';
-import * as StationMaintenanceService from '../../../../services/stationMaintenancesService';
 import { mockMaintenanceHoursSummary } from '../../../../services/__mocks__/stationMaintenancesService';
 import { store } from '../../../../redux/store';
 
-const mockGetStationMaintenances = jest.spyOn(
-  StationMaintenanceService,
-  'getBrokerMaintenancesSummary'
-);
 describe('<StationMaintenancesHoursSummary />', () => {
   test('render StationMaintenancesHoursSummary', () => {
-    mockGetStationMaintenances.mockResolvedValueOnce(mockMaintenanceHoursSummary);
     render(
       <Provider store={store}>
-        <StationMaintenancesHoursSummary />
+        <StationMaintenancesHoursSummary hoursSummary={mockMaintenanceHoursSummary}/>
       </Provider>
     );
   });
   test('render StationMaintenancesHoursSummary error getBrokerMaintenancesSummary', () => {
-    mockGetStationMaintenances.mockRejectedValueOnce("error");
     render(
       <Provider store={store}>
-        <StationMaintenancesHoursSummary />
+        <StationMaintenancesHoursSummary hoursSummary={mockMaintenanceHoursSummary}/>
       </Provider>
     );
   });

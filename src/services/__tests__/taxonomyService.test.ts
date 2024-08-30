65a673639fd10c58e62d6efd71fbe982e3c6a773
svariant <samuele.varianti@nttdata.com>
2024-08-30 17:03:06 +0200

    [PPANTT-99] fix: Coverage unit tests

--- src/services/__tests__/taxonomyService.test.ts
+++ src/services/__tests__/taxonomyService.test.ts
@@ -1,16 +1,41 @@
-import {getTaxonomies, getTaxonomyGroups} from '../taxonomyService';
-import {mockedTaxonomyGroups, mockedTaxonomy} from '../__mocks__/taxonomyService';
+import { getTaxonomies, getTaxonomyGroups } from '../taxonomyService';
+import { mockedTaxonomyGroups, mockedTaxonomy } from '../__mocks__/taxonomyService';
+import { BackofficeApi } from '../../api/BackofficeClient';
 
-describe('getTaxonomyGroups test', () => {
-    test('Test getTaxonomyGroups', async () => {
-        const taxonomyGroups = await getTaxonomyGroups();
-        expect(taxonomyGroups).toMatchObject(mockedTaxonomyGroups);
-    });
+describe('TaxonomiesService test client', () => {
+  const OLD_ENV = process.env;
+  beforeEach(() => {
+    jest.resetModules();
+    process.env = { ...OLD_ENV, REACT_APP_API_MOCK_BACKOFFICE: 'false' };
+  });
+
+  afterAll(() => {
+    process.env = OLD_ENV;
+  });
+
+  test('Test getTaxonomyGroups', async () => {
+    const spyOn = jest
+      .spyOn(BackofficeApi.taxonomies, 'getTaxonomyGroups')
+      .mockReturnValue(new Promise((resolve) => resolve(mockedTaxonomyGroups)));
+    expect(getTaxonomyGroups()).resolves.not.toThrow();
+    expect(spyOn).toBeCalledTimes(1);
+  });
+  test('Test getTaxonomies', async () => {
+    const spyOn = jest
+      .spyOn(BackofficeApi.taxonomies, 'getTaxonomies')
+      .mockReturnValue(new Promise((resolve) => resolve(mockedTaxonomy)));
+    expect(getTaxonomies('', '', '', false)).resolves.not.toThrow();
+    expect(spyOn).toBeCalledTimes(1);
+  });
 });
 
-describe('getTaxonomies test', () => {
-    test('Test getTaxonomies', async () => {
-        const taxonomies = await getTaxonomies("", "", "", false);
-        expect(taxonomies).toMatchObject(mockedTaxonomy);
-    });
+describe('TaxonomiesService test mocked', () => {
+  test('Test getTaxonomyGroups', async () => {
+    const taxonomyGroups = await getTaxonomyGroups();
+    expect(taxonomyGroups).toMatchObject(mockedTaxonomyGroups);
+  });
+  test('Test getTaxonomies', async () => {
+    const taxonomies = await getTaxonomies('', '', '', false);
+    expect(taxonomies).toMatchObject(mockedTaxonomy);
+  });
 });

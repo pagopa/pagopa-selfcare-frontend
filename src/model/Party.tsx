530b0647575851d9b320256198eed417a567c192
svariant <samuele.varianti@nttdata.com>
2024-10-11 15:22:39 +0200

    [PPANTT-55] fix: App build

--- src/model/Party.tsx
+++ src/model/Party.tsx
@@ -1,7 +1,7 @@
-import {InstitutionDetail} from '../api/generated/portal/InstitutionDetail';
-import {Institution} from '../api/generated/portal/Institution';
-import {PspData} from '../api/generated/portal/PspData';
-import {ENV} from '../utils/env';
+import { InstitutionDetail } from '../api/generated/portal/InstitutionDetail';
+import { Institution } from '../api/generated/portal/Institution';
+import { PspData } from '../api/generated/portal/PspData';
+import { ENV } from '../utils/env';
 import { InstitutionBase } from '../api/generated/portal/InstitutionBase';
 
 export type SelfcareRole = 'ADMIN' | 'LIMITED';
@@ -9,98 +9,100 @@ export type PartyRole = 'DELEGATE' | 'MANAGER' | 'OPERATOR' | 'SUB_DELEGATE';
 export type UserStatus = 'PENDING' | 'ACTIVE' | 'SUSPENDED';
 
 export type Party = {
-    partyId: string;
-    externalId: string;
-    originId: string;
-    origin: string;
-    description: string;
-    digitalAddress: string;
-    status: UserStatus;
-    roles: Array<UserRole>;
-    category?: string;
-    urlLogo?: string;
-    fiscalCode: string;
-    registeredOffice: string;
-    institutionType?: string;
-    pspData?: PspData;
+  partyId: string;
+  externalId: string;
+  originId: string;
+  origin: string;
+  description: string;
+  digitalAddress: string;
+  status: UserStatus;
+  roles: Array<UserRole>;
+  category?: string;
+  urlLogo?: string;
+  fiscalCode: string;
+  registeredOffice: string;
+  institutionType?: string;
+  pspData?: PspData;
 };
 
 export type UserRole = {
-    partyRole: PartyRole;
-    roleKey: string;
-    roleLabel: string;
+  partyRole: PartyRole;
+  roleKey: string;
+  roleLabel: string;
 };
 
 export type BaseParty = {
-    partyId: string;
-    description: string;
-    status: UserStatus;
-    roles: Array<UserRole>;
-    urlLogo?: string;
-    parentDescription?: string;
-  };
-
-const buildUrlLog = (partyId: string) =>
-    `${ENV.URL_INSTITUTION_LOGO.PREFIX}${partyId}${ENV.URL_INSTITUTION_LOGO.SUFFIX}`;
-
-export const institutionResource2Party = (institutionResource: InstitutionDetail): Party => {
-
-    const urlLogo = institutionResource.id && buildUrlLog(institutionResource.id);
-    return {
-        partyId: institutionResource.id,
-        externalId: institutionResource.external_id,
-        originId: institutionResource.origin_id,
-        origin: institutionResource.origin,
-        description: institutionResource.name,
-        digitalAddress: institutionResource.mail_address!,
-        status: institutionResource.status as 'ACTIVE' | 'PENDING',
-        roles: institutionResource.user_product_roles.map(
-            (u) => ({partyRole: u.product_role === 'admin' || 'admin-psp' ?
-             'DELEGATE' : 'OPERATOR', roleKey: u.product_role, roleLabel: u.product_role_label} as UserRole)
-        ),
-        urlLogo,
-        fiscalCode: institutionResource.tax_code,
-        registeredOffice: institutionResource.address!,
-        institutionType: institutionResource.institution_type,
-        pspData: institutionResource.psp_data,
-    };
+  partyId: string;
+  description: string;
+  status: UserStatus;
+  roles: Array<UserRole>;
+  urlLogo?: string;
+  parentDescription?: string;
 };
 
-export const institutionDetailResource2Party = (
-    institutionResource: Institution
-): Party => {
-    const urlLogo = institutionResource.id && buildUrlLog(institutionResource.id);
-    return {
-        partyId: institutionResource.id,
-        externalId: institutionResource.external_id,
-        originId: institutionResource.origin_id,
-        origin: institutionResource.origin,
-        description: institutionResource.description,
-        digitalAddress: institutionResource.digital_address!,
-        status: 'ACTIVE', // 'ACTIVE' | 'PENDING',
-        roles: [] /* institutionResource.userProductRoles.map(
+const buildUrlLog = (partyId: string) =>
+  `${ENV.URL_INSTITUTION_LOGO.PREFIX}${partyId}${ENV.URL_INSTITUTION_LOGO.SUFFIX}`;
+
+const getRoles = (institutionResource: InstitutionDetail | InstitutionBase) =>
+  institutionResource.user_product_roles.map(
+    (u) =>
+      ({
+        partyRole:
+          u.product_role === 'admin' || u.product_role === 'admin-psp' ? 'DELEGATE' : 'OPERATOR',
+        roleKey: u.product_role,
+        roleLabel: u.product_role_label,
+      }) as UserRole
+  );
+
+export const institutionResource2Party = (institutionResource: InstitutionDetail): Party => {
+  const urlLogo = institutionResource.id && buildUrlLog(institutionResource.id);
+  return {
+    partyId: institutionResource.id,
+    externalId: institutionResource.external_id,
+    originId: institutionResource.origin_id,
+    origin: institutionResource.origin,
+    description: institutionResource.name,
+    digitalAddress: institutionResource.mail_address!,
+    status: institutionResource.status as 'ACTIVE' | 'PENDING',
+    roles: getRoles(institutionResource),
+    urlLogo,
+    fiscalCode: institutionResource.tax_code,
+    registeredOffice: institutionResource.address!,
+    institutionType: institutionResource.institution_type,
+    pspData: institutionResource.psp_data,
+  };
+};
+
+export const institutionDetailResource2Party = (institutionResource: Institution): Party => {
+  const urlLogo = institutionResource.id && buildUrlLog(institutionResource.id);
+  return {
+    partyId: institutionResource.id,
+    externalId: institutionResource.external_id,
+    originId: institutionResource.origin_id,
+    origin: institutionResource.origin,
+    description: institutionResource.description,
+    digitalAddress: institutionResource.digital_address!,
+    status: 'ACTIVE', // 'ACTIVE' | 'PENDING',
+    roles: [] /* institutionResource.userProductRoles.map(
       (u) => ({ partyRole: u.product_role, roleKey: u.product_role, roleLabel: u.product_role_label } as UserRole)
     ), */,
-        urlLogo,
-        fiscalCode: institutionResource.tax_code,
-        registeredOffice: institutionResource.address!,
-        institutionType: institutionResource.institution_type,
-    };
+    urlLogo,
+    fiscalCode: institutionResource.tax_code,
+    registeredOffice: institutionResource.address!,
+    institutionType: institutionResource.institution_type,
+  };
 };
 
 export const institutionBaseResource2BaseParty = (
-    institutionResource: InstitutionBase
-  ): BaseParty => {
-    const urlLogo = institutionResource.id && buildUrlLog(institutionResource.id);
-    return {
-      partyId: institutionResource.id ?? '',
-      description: institutionResource.name ?? '',
-      // status: institutionResource.status as 'ACTIVE' | 'PENDING',
-      status: 'ACTIVE',
-      roles: institutionResource.user_product_roles.map(
-            (u) => ({partyRole: u.product_role === 'admin' || 'admin-psp' ?
-             'DELEGATE' : 'OPERATOR', roleKey: u.product_role, roleLabel: u.product_role_label} as UserRole)
-      ),      
-      urlLogo,
-    };
+  institutionResource: InstitutionBase
+): BaseParty => {
+  const urlLogo = institutionResource.id && buildUrlLog(institutionResource.id);
+  return {
+    partyId: institutionResource.id ?? '',
+    description: institutionResource.name ?? '',
+    // status: institutionResource.status as 'ACTIVE' | 'PENDING',
+    status: 'ACTIVE',
+    roles: getRoles(institutionResource),
+    urlLogo,
   };
+};

06c4e60ce75f41b561ccf4f314ee14668af527ea
Samuele Varianti <128470180+svariant@users.noreply.github.com>
2025-04-14 09:33:10 +0200

    feat: [PIDM-441] Update e2e test after jwt taxCode verification (#804)

--- integration-test/tests/utils/e2eUtils.ts
+++ integration-test/tests/utils/e2eUtils.ts
@@ -16,36 +16,47 @@ export const MARKETPLACE_BE_URL = BASE_BE_URL + '/afm/marketplace-service/v1';
 export const PSP_DEMO_DIRECT = '99999000011';
 export const PSP_DEMO_DIRECT_CODE = 'ABI50004';
 
-export const ORG_ID = {
-  COMUNE_FROSINONE: '5b46b1d5-63fa-43fb-adc4-21093b1d6cd6',
-  EC_DEMO_DIRECT: '21079fe3-b5eb-4978-bef0-5588d11ab53f',
-  PSP_DEMO_DIRECT: '5b7130fb-dbe1-4e3b-b02b-2a9cfaf54602',
+export type ORG_TYPE = {
+  id: string;
+  taxCode: string;
 };
 
-export async function login(page: Page, org_id: string = ORG_ID.COMUNE_FROSINONE, isOperator?:boolean) {
+export const ORG = {
+  EC_DEMO_DIRECT: { id: '21079fe3-b5eb-4978-bef0-5588d11ab53f', taxCode: '99999000013' },
+  PSP_DEMO_DIRECT: { id: '5b7130fb-dbe1-4e3b-b02b-2a9cfaf54602', taxCode: '99999000011' },
+};
+
+export async function login(
+  page: Page,
+  org: ORG_TYPE = ORG.EC_DEMO_DIRECT,
+  isOperator?: boolean
+) {
   const feURL: string = process.env.FE_URL ?? DEV_URL;
-  const jwt = createJWT(org_id, isOperator); // id Comune di Frosinone
+  const jwt = createJWT(org, isOperator); // id Comune di Frosinone
   await page.goto(feURL + '#logged=forced');
-  await page.evaluate(({jwt, isOperator}) => {
-    const user = {
-      uid: '5096e4c6-25a1-45d5-9bdf-2fb974a7c1c8',
-      name: 'Anselmo',
-      surname: 'Sartori',
-      email: isOperator ? 'operatorePagopa@test.it' : 'furiovitale@martino.it'
-    };
-    const tos = {
-      id: '5096e4c6-25a1-45d5-9bdf-2fb974a7c1c8',
-      timestamp: '2024-02-20T14:28:10.041Z',
-    };
-    window.localStorage.setItem('acceptTOS', JSON.stringify(tos));
-    window.localStorage.setItem('user', JSON.stringify(user));
-    window.localStorage.setItem('token', jwt);
-  }, {jwt, isOperator});
+  await page.evaluate(
+    ({ jwt, isOperator }) => {
+      const user = {
+        uid: '5096e4c6-25a1-45d5-9bdf-2fb974a7c1c8',
+        name: 'Anselmo',
+        surname: 'Sartori',
+        email: isOperator ? 'operatorePagopa@test.it' : 'furiovitale@martino.it',
+      };
+      const tos = {
+        id: '5096e4c6-25a1-45d5-9bdf-2fb974a7c1c8',
+        timestamp: '2024-02-20T14:28:10.041Z',
+      };
+      window.localStorage.setItem('acceptTOS', JSON.stringify(tos));
+      window.localStorage.setItem('user', JSON.stringify(user));
+      window.localStorage.setItem('token', jwt);
+    },
+    { jwt, isOperator }
+  );
   await page.waitForTimeout(1000);
   await page.goto(feURL, { waitUntil: 'load' });
 }
 
-export async function isOperator(page: Page){
+export async function isOperator(page: Page) {
   await page.getByText('Operatore PagoPA');
 }
 
@@ -55,11 +66,11 @@ export async function checkReturnHomepage(page: Page) {
 }
 
 export async function changeToEcUser(page: Page, isOperator?: boolean) {
-  await login(page, ORG_ID.EC_DEMO_DIRECT, isOperator);
+  await login(page, ORG.EC_DEMO_DIRECT, isOperator);
 }
 
 export async function changeToPspUser(page: Page, isOperator?: boolean) {
-  await login(page, ORG_ID.PSP_DEMO_DIRECT, isOperator);
+  await login(page, ORG.PSP_DEMO_DIRECT, isOperator);
 }
 
 export function getTodayDate() {
@@ -84,7 +95,7 @@ export function getDateAfterThreeDays() {
   const currentDate = add(new Date(), { days: 3 });
   currentDate.setHours(0, 0, 0, 0);
   const dd = String(currentDate.getDate()).padStart(2, '0');
-  const mm = String(currentDate.getMonth() as number + 1).padStart(2, '0');
+  const mm = String((currentDate.getMonth() as number) + 1).padStart(2, '0');
   const yyyy = currentDate.getFullYear();
 
   return dd + '/' + mm + '/' + String(yyyy);

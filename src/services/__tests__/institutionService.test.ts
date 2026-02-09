96be30877941d7efffe66e3e5f20202165d45ad6
pietro.tota <pietro.tota@pagopa.it>
2026-02-09 17:04:44 +0100

    feat: add tests for institution service

--- src/services/__tests__/institutionService.test.ts
+++ src/services/__tests__/institutionService.test.ts
@@ -1,9 +1,13 @@
-import {BackofficeApi} from '../../api/BackofficeClient';
+import { BackofficeApi } from '../../api/BackofficeClient';
+import { ConsentEnum } from '../../api/generated/portal/ServiceConsentInfo';
 import {
+    getServicesConsentsOptOutResponseMock,
+    getServicesConsentsResponseMock,
     mockedDelegatedPSP,
     mockedInstitutionDetailResource,
+    mockedServiceConsentResponse,
 } from '../__mocks__/institutionsService';
-import {getBrokerDelegation, getInstitutionFullDetail, getInstitutions} from '../institutionService';
+import { getBrokerDelegation, getInstitutionFullDetail, getInstitutions, getServiceConsents, saveServiceConsent } from '../institutionService';
 
 describe('InstitutionService test mocked', () => {
     test('Test getBrokerDelegation', async () => {
@@ -26,14 +30,14 @@ describe('InstitutionService test client', () => {
     const OLD_ENV = process.env;
     beforeEach(() => {
         jest.resetModules();
-        process.env = {...OLD_ENV, REACT_APP_API_MOCK_BACKOFFICE: 'false'};
+        process.env = { ...OLD_ENV, REACT_APP_API_MOCK_BACKOFFICE: 'false' };
     });
 
     afterAll(() => {
         process.env = OLD_ENV;
     });
 
-    test('Test InstitutionService', async () => {
+    test('Test InstitutionService.getBrokerDelegation', async () => {
         const spyOn = jest
             .spyOn(BackofficeApi.institutions, 'getBrokerDelegation')
             .mockReturnValue(Promise.resolve(mockedDelegatedPSP));
@@ -41,7 +45,7 @@ describe('InstitutionService test client', () => {
         expect(spyOn).toBeCalledTimes(1);
     });
 
-    test('Test InstitutionService', async () => {
+    test('Test InstitutionService.getInstitutions', async () => {
         const spyOn = jest
             .spyOn(BackofficeApi.institutions, 'getInstitutions')
             .mockReturnValue(Promise.resolve(mockedInstitutionDetailResource));
@@ -49,11 +53,31 @@ describe('InstitutionService test client', () => {
         expect(spyOn).toBeCalledTimes(1);
     });
 
-    test('Test InstitutionService', async () => {
+    test('Test InstitutionService.getInstitutionFullDetail', async () => {
         const spyOn = jest
             .spyOn(BackofficeApi.institutions, 'getInstitutionFullDetail')
             .mockReturnValue(Promise.resolve(mockedInstitutionDetailResource));
         expect(getInstitutionFullDetail('taxCode')).resolves.not.toThrow();
         expect(spyOn).toBeCalledTimes(1);
+    });
+
+    test('Test InstitutionService.saveServiceConsent', async () => {
+        const institutionId = "institutionId";
+        const serviceId = "RTP";
+        const consent = ConsentEnum.OPT_IN;
+        const spy = jest
+            .spyOn(BackofficeApi.institutions, 'saveServiceConsent')
+            .mockReturnValue(Promise.resolve(mockedServiceConsentResponse(consent, new Date())));
+        expect(saveServiceConsent(institutionId, serviceId, consent)).resolves.not.toThrow();
+        expect(spy).toHaveBeenNthCalledWith(1, institutionId, serviceId, consent);
+    });
+
+    test('Test InstitutionService.getServiceConsents', async () => {
+        const institutionId = "institutionId";
+        const spy = jest
+            .spyOn(BackofficeApi.institutions, 'getServiceConsents')
+            .mockReturnValue(getServicesConsentsOptOutResponseMock());
+        expect(getServiceConsents(institutionId)).resolves.not.toThrow();
+        expect(spy).toHaveBeenNthCalledWith(1, institutionId);
     });
 });

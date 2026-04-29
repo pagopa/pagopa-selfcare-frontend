5c73b9b56d8c0f8a0839fff413b92aef4ed2cdea
svariant <samuele.varianti@nttdata.com>
2026-04-29 17:20:38 +0200

    Revert "feat: [PIDM-1631] PaymentReceiptTable with generalized table"
    This reverts commit b885ea64f8520cd915b9948edac518405d88f8a6.

--- src/pages/paymentsReceipts/list/PaymentsReceiptsTable.tsx
+++ src/pages/paymentsReceipts/list/PaymentsReceiptsTable.tsx
@@ -5,7 +5,7 @@ import {useErrorDispatcher, useLoading} from '@pagopa/selfcare-common-frontend';
 import {useTranslation} from 'react-i18next';
 import {useState, useEffect, ChangeEvent} from 'react';
 import {Pagination} from '@mui/material';
-import TableDataGrid from '../../../components/Table/TableDataGrid';
+import { CustomDataGrid } from '../../../components/Table/TableDataGrid';
 import {useAppSelector} from '../../../redux/hooks';
 import {partiesSelectors} from '../../../redux/slices/partiesSlice';
 import {LOADING_TASK_PAYMENTS_RECEIPTS} from '../../../utils/constants';
@@ -107,6 +107,7 @@ export default function PaymentsReceiptsTable({
     }, [searchTrigger]);
 
     const columns: Array<GridColDef> = buildColumnDefs(t, downloadReceiptXML);
+    // TODO generalize table box
     return (
         <Box
             id="paymentsReceiptsTable"
@@ -117,15 +118,43 @@ export default function PaymentsReceiptsTable({
             }}
             justifyContent="start"
         >
-            <TableDataGrid
-                componentPath={"paymentsReceiptsPage"}
-                rows={receiptsList.results ? [...receiptsList.results] : []}
-                columns={columns}
-                totalPages={receiptsList?.totalPages}
-                page={page}
-                handleChangePage={(newPage: number) => getReceipts(newPage)}
-                getRowId={(el) => el.iuv}
-            />
+            {!receiptsList?.results || receiptsList.results.length === 0 ? (
+                <TableEmptyState componentName="paymentsReceiptsPage"/>
+            ) : (
+                <div data-testid="data-grid">
+                    <CustomDataGrid
+                        disableColumnFilter
+                        disableColumnSelector
+                        disableDensitySelector
+                        disableSelectionOnClick
+                        onPageChange={(newPage) => getReceipts(newPage - 1)}
+                        autoHeight={true}
+                        className="CustomDataGrid"
+                        components={{
+                            Pagination: () => (
+                                <>
+                                    <Pagination
+                                        color="primary"
+                                        count={receiptsList?.totalPages ?? 1}
+                                        page={page + 1}
+                                        onChange={(_event: ChangeEvent<unknown>, value: number) =>
+                                            getReceipts(value - 1)
+                                        }
+                                    />
+                                </>
+                            ),
+                        }}
+                        columnBuffer={5}
+                        columns={columns}
+                        headerHeight={headerHeight}
+                        hideFooterSelectedRowCount={true}
+                        rowCount={receiptsList?.results?.length}
+                        getRowId={(el) => el.iuv}
+                        rowHeight={rowHeight}
+                        rows={receiptsList.results ?? []}
+                    />
+                </div>
+            )}
         </Box>
     );
 }
